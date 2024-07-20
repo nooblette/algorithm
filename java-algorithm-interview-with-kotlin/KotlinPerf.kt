62d8acdd4d009125511dca1f2d9ebb6214f479d1
nooblette <min9hyuk@gmail.com>
2024-07-20 15:11:14 +0900

    코틀린의 자료형 컴파일 방식

---
+++ java-algorithm-interview-with-kotlin/KotlinPerf.kt
@@ -0,0 +1,14 @@
+fun main() {
+    // IntArray 1억개 삽입
+    val intElements = IntArray(100000000)  // 배열을 크기 1억으로 초기화
+    for (i in 0 until 100000000 - 1) {
+        intElements[i] = 1
+    }
+    intElements[100000000 - 1] = 2
+
+    // IntArray 1억개 중 찾기
+    var index = 0
+    while (2 != intElements[index]) {
+        index++
+    }
+}
