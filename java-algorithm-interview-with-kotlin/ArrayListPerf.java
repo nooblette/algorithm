728caf29c47e6b0b733331a34909697dfcac7e09
nooblette <min9hyuk@gmail.com>
2024-07-20 17:07:27 +0900

    자바 컬렉션 프레임워크의 실행 속도

---
+++ java-algorithm-interview-with-kotlin/ArrayListPerf.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListPerf {
+    public static void main(String[] args) {
+        List<Integer> list1 = new ArrayList<>();;
+
+        // 배열 삽입 시간 측정 시작
+        long startTimeInsert = System.nanoTime();
+        for (int i = 0; i < 100000000 - 1; i++) {
+            list1.add(1);
+        }
+	list1.add(2);
+        
+        // 배열 삽입 시간 측정 종료
+        long endTimeInsert = System.nanoTime();
+        long durationInsert = endTimeInsert - startTimeInsert;
+        System.out.println("삽입 시간: " + durationInsert + " 나노초");
+
+        // 배열 찾기 시간 측정 시작
+        long startTimeSearch = System.nanoTime();
+        int index = 0;
+        while (2 != list1.get(index)) {
+            index++;
+        }
+        // 배열 찾기 시간 측정 종료
+        long endTimeSearch = System.nanoTime();
+        long durationSearch = endTimeSearch - startTimeSearch;
+        System.out.println("찾기 시간: " + durationSearch + " 나노초");
+    }
+}
