96a854a427d2fc699afe73841f406b043c477a63
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-07-22 05:48:57 +0900

    Time: 4 ms (63.14%), Space: 44.4 MB (95.07%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,20 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // 입력 값 배열을 처음부터 순회
+        Map<Integer, Integer> numsMap = new HashMap<>();
+        
+        // 키와 값을 바꿔서 맵으로 저장
         for(int i = 0; i < nums.length; i++) {
-            // 입력 값 배열을 그 다음부터 순회
-            for(int j = i + 1; j < nums.length; j++) {
-                // 두 값의 합을 비교해 target과 일치하는 경우 정답으로 리턴
-                if(nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+            numsMap.put(nums[i], i);
+        }
+        
+        // target에서 첫 번째 수를 뺀 결과를 키로 조회, 현재 인덱스가 아닌 경루 정답으로 처리
+        for(int i = 0; i < nums.length; i++) {
+            if(numsMap.containsKey(target - nums[i]) && i != numsMap.get(target - nums[i])) {
+                return new int[]{i, numsMap.get(target - nums[i])};
             }
         }
-
+        
+        // 항상 정답이 있으므로 null이 반환되는 경우는 없음
         return null;
     }
 }
