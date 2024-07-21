24f25b6fbb66afa6e0e9242f9690260082d187a9
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-07-21 15:55:22 +0900

    Time: 14 ms (34.56%), Space: 44.8 MB (27.49%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,27 +1,10 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int start = 0;
-        int end = s.length() - 1;
-        
-        // 자료 중앙으로 이동해 나가다 겹치는 지점에 도달하면 종료
-        while(start < end) {
-            // 영숫자인지 판별, 유효하지 않으면 한 칸 이동한다.
-            if(!Character.isLetterOrDigit(s.charAt(start))){
-                start++;
-            } else if(!Character.isLetterOrDigit(s.charAt(end))){
-                end--;
-            } else {
-                // 하나라도 일치하지 않으면 팰린드롬이 아니므로 false 반환
-                if(Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
-                    return false;
-                }
-                
-                // 팰린드롬이 맞다면 다음 인덱스에 대해 반복 진행
-                start++;
-                end--;
-            }
-        }
-        // 무사히 종료한 경우, true 반환
-        return true;
+        // 정규표현식으로 영숫자만을 걸러낸다.
+        String filtered_s = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        // 문자열을 뒤집는다.
+        String revesed_filtered_s = new StringBuilder(filtered_s).reverse().toString();
+        // 팰린드롬 검증
+        return filtered_s.equals(revesed_filtered_s);
     }
 }
