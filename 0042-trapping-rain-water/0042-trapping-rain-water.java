efcbdeb68ffcb2dffa805afdbc213063b9f882c9
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-07-22 06:25:50 +0900

    Time: 3 ms (13.23%), Space: 45.2 MB (97.95%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,23 +1,28 @@
 class Solution {
     public int trap(int[] height) {
+        Deque<Integer> stack = new ArrayDeque<>();
         int volume = 0;
-        int left = 0;
-        int right = height.length - 1;
-        int leftMax = height[left];
-        int rightMax = height[right];
         
-        while(left < right) {
-            leftMax = Math.max(leftMax, height[left]);
-            rightMax = Math.max(rightMax, height[right]);
-            
-            if(leftMax <= rightMax) {
-                // 왼쪽 막대 최대 높이와의 차이를 더하고 왼쪽 포인터로 이동
-                volume += leftMax - height[left];
-                left++;
-            } else {
-                volume += rightMax - height[right];
-                right--;
+        for(int i = 0; i < height.length; i++) {
+            // 변곡점을 만나는 경우
+            while(!stack.isEmpty() && height[i] > height[stack.peek()]) {
+                Integer top = stack.pop();
+                
+                if(stack.isEmpty()) {
+                    break;
+                }
+                
+                // 스택의 마지막 위치까지를 거리로 계산
+                int distance = i - stack.peek() - 1;
+                
+                // 현재 높이 또는 스택의 마지막 위치 높이중 낮은 값에 방금 꺼낸 높이의 차이를 물 높이로 지정
+                int waters = Math.min(height[i], height[stack.peek()]) - height[top];
+                
+                // 물에 쌓이는 일은 거리와 물 높이의 곱
+                volume += distance * waters;
             }
+            
+            stack.push(i);
         }
         
         return volume;
