825fd6c7aa6d40af026fc703385482ae8f778cb7
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-08-11 13:14:43 +0900

    Time: 1 ms (100.00%), Space: 44.5 MB (26.62%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -8,52 +8,46 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-import java.math.*;
-
 class Solution {
-    public ListNode reverseList(ListNode head) {
-        ListNode prev = null, node = head;
-
-        while(node != null) {
-            ListNode next = node.next;
-            node.next = prev;
-            prev = node;
-            node = next;
-        }
-
-        return prev;
-    }
-    
-    public BigInteger toBigInt(ListNode node) {
-        String val = "";
-        while(node != null) {
-            val += node.val;
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // 값을 계산할 임시 노드 선언
+        ListNode node = new ListNode(0);
+        
+        // 임시 노드를 첫번째 노드로 선언
+        ListNode root = node;
+        
+        // 자릿수의 합(sum), 자리올림수(carry), 나머지(remainder)를 저장할 변수 선언
+        int sum, carry = 0, remainder = 0;
+        
+        // 두 연결리스트 l1과 l2를 끝까지 순회, 자리 올림수도 0이 될때까지 진행
+        while(l1 != null || l2 != null || carry != 0) {
+            sum = 0;
+            
+            // 첫 번째 연결리스트 합산 및 진행
+            if(l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            
+            // 두 번째 연결리스트 합산 및 진행
+            if(l2 != null) {
+                sum += l2.val;
+                l2 = l2.next; 
+            }
+            
+            // 자리올림수와 나머지 계산
+            remainder = (sum + carry) % 10;
+            carry = (sum + carry) / 10;
+            
+            // 나머지 값을 현재 노드의 다음 노드로 지정
+            node.next = new ListNode(remainder);
+            
+            // 다음 노드로 이동
             node = node.next;
         }
-
-        return new BigInteger(val);
-    }
-    
-    public ListNode toReversedLinkedList(BigInteger val) {
-        ListNode prev = null, node = null;
-        for(char c : String.valueOf(val).toCharArray()) {
-            node = new ListNode(Character.getNumericValue(c));
-            node.next = prev;
-            prev = node;
-        }
-
-        return node;
-    }
-
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        // 두 연결리스트 뒤집기
-        ListNode reversedL1 = reverseList(l1);
-        ListNode reversedL2 = reverseList(l2);
-
-        // 뒤집은 연결리스트의 합 계산
-        BigInteger result = toBigInt(reversedL1).add(toBigInt(reversedL2));
-
-        // 결과를 다시 역순 연결리스트로 변환
-        return toReversedLinkedList(result);
+        
+        // 첫번째 노드(root)는 임시노드, 따라서 다음 노드를 결과로 반환
+        return root.next;
+        
     }
 }
