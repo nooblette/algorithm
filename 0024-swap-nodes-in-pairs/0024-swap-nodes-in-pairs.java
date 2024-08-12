95f731ded3977ba0ddb92875e9023c48e83998d9
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-08-12 21:55:19 +0900

    Time: 0 ms (100.00%), Space: 41.1 MB (47.39%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -10,21 +10,30 @@
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        // 스왑을 진행할 노드 선언
-        ListNode node = head;
+        // 값을 계산할 임시 노드
+        ListNode node = new ListNode(0);
         
-        // 현재 노드와 다음 노드가 진행하면 계속 반복
-        while(node != null && node.next != null) {
-            // 현재 노드와 다음 노드의 값을 스왑
-            int temp = node.val;
-            node.val = node.next.val;
-            node.next.val = temp;
+        // 첫번째 노드를 임시 노드로 선언
+        ListNode root = node;
+        
+        // 첫번째 노드의 다음 노드를 head 노드로 선언
+        node.next = head;
+        
+        // 다음 노드와 다음 노드가 있는 동안 반복
+        while(node.next != null && node.next.next != null) {
+            ListNode a = node.next;
+            ListNode b = node.next.next;
+            
+            // 연결 리스트의 연결 구조 변경
+            a.next = b.next;
+            node.next = b;
+            b.next = a;
             
             // 두 칸 앞으로 이동
             node = node.next.next;
         }
         
-        // 첫번째 노드를 결과로 반환
-        return head;
+        // 루트 노드는 임시 노디이므로 루트 노드의 다음 노드를 결과로 반환
+        return root.next;
     }
 }
