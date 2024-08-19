1bd3fef1d6a681216aba12ee98b0bbd5f958df36
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-08-19 22:07:30 +0900

    [level 2] Title: 더 맵게, Time: 1591.59 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626#) 
 
 ### 성능 요약
 
-메모리: 123 MB, 시간: 1393.12 ms
+메모리: 123 MB, 시간: 1591.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 24일 20:47:41
+2024년 08월 19일 22:07:28
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -3,26 +3,33 @@ import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int s : scoville){
-            pq.offer(s);
+        
+        // scoville 원소를 우선순위 큐에 삽입
+        for(int s : scoville) {
+            // 삽입하면서 스코빌을 기준으로 오름차순 정렬된다.
+            pq.add(s);
         }
         
+        // 두 음식을 꺼낼 수 있는 동안 반복
         int answer = 0;
-        while(!pq.isEmpty()) {
-            int first = pq.poll();
-            if(first >= K) {
-                break;
-            }
-            
-            if(pq.isEmpty()) {
-                answer = -1;
-                break;
-            }
-            
-            int second = pq.poll();
-            pq.offer(first + (second * 2));
-            answer += 1;
+        
+        // 섞을 필요가 없는 경우
+        if(pq.peek() >= K) {
+            return answer;
         }
-        return answer;
+        
+        while(pq.size() >= 2) {
+            // 두 음식을 섞어서 새로운 스코빌 지수를 갖는 음식을 만든다. 그 후 다시 우선순위 큐에 넣는다.
+            pq.add(pq.poll() + pq.poll() * 2);
+            answer++;
+            
+            // 모든 음식의 스코빌 지수가 K 이상이 되면 반복 횟수를 반환한다.
+            if(pq.peek() >= K) {
+                return answer;
+            }
+        }
+        
+        // 모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우에 -1을 return한다.
+        return -1;
     }
 }
