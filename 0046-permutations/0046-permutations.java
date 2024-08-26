4f6cb24d0f62a9469370f8c0c05f0eb409bef444
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-08-26 22:29:45 +0900

    Time: 5 ms (5.05%), Space: 45.2 MB (6.47%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,35 +1,43 @@
 class Solution {
-    public void dfs(List<Integer> path, List<List<Integer>> results, int[] nums) {
-        // 모든 숫자를 방문한 경우
-        if(path.size() == nums.length) {
-            // 방문한 경로를 새로운 변수로 선언하여 path에 추가
-            // path를 바로 추가하면 참조값이 추가되어 for loop 종료시 비어있는 리스트를 가리키게 된다.
-            results.add(new ArrayList<>(path));
-            return;
+        public void dfs(
+        List<List<Integer>> results, 
+        List<Integer> prevElements, 
+        List<Integer> elements
+    ) {
+        // 리프노드에 도달하면 결과에 추가
+        if(elements.isEmpty()) {
+            results.add(prevElements.stream().collect(Collectors.toList()));
         }
-        
-        for(int num : nums) {
-            // 현재 방문한 경로에 없다면
-            if(!path.contains(num)) {
-                // 현재까지 방문한 경로에 추가
-                path.add(num);
 
-                // dfs 탐색 시작
-                dfs(path, results, nums);
+        // 전달받은 elements 탐색
+        for(Integer e : elements) {
+            // 전달받은 엘리먼트에서 현재 엘리먼트를 제외하고 nextElements를 새롭게 구성
+            // elements를 바로 nextElements에 할당하면 동일한 참조값을 가리키게되어 별도로 리스트를 선언한다.
+            List<Integer> nextElements = new ArrayList<>(elements);
+            nextElements.remove(e);
 
-                // 현재 방문한 경로에서 제거, 인덱스가 아닌 값을 기준으로 제거하기 위해 Integer 클래스로 전달
-                path.remove(Integer.valueOf(num));
-            }
+            // prevElements 기존 값에 현재 엘리먼트 추가(방문한 경로를 저장하는 역할)
+            prevElements.add(e);
+
+            // dfs g호출
+            dfs(results, prevElements, nextElements);
+
+            // 돌아온 이후 prevElements 현재 엘리먼트 삭제
+            prevElements.remove(e);
         }
     }
     
     public List<List<Integer>> permute(int[] nums) {
-        // 결과를 담을 변수
+        // 결과 저장 리스트 선언
         List<List<Integer>> results = new ArrayList<>();
         
-        // 현재까지 방문한 경로 저장
-        List<Integer> path = new ArrayList<>(); 
-        dfs(path, results, nums);
+        // int[]를 List<Integer>로 변경
+        List<Integer> lst = Arrays.stream(nums)
+                .boxed()
+                .collect(Collectors.toList());
+        
+        // dfs 시작
+        dfs(results, new ArrayList<>(), lst);
         
         return results;
     }
