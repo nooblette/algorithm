38d89574365027c95bb69bdd36c79229b9a6347e
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-08-30 10:03:34 +0900

    Time: 3 ms (29.15%), Space: 44.7 MB (35.30%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,39 +1,31 @@
 class Solution {
-    public void dfs(List<List<Integer>> results, List<Integer> path, int[] candidates, int sum, int target, int start) {
-        // 숫자 조합의 합이 target이 되는 경우 results에 추가
-        if(sum == target) {
+    public void dfs(List<List<Integer>> results, int[] candidates, int target, int index, Deque<Integer> path) {
+        // 0보다 작다면 목표값을 넘었으므로 리턴
+        if(target < 0) {
+            return;
+        }
+
+        // 0이면 목표값에 도달했으므로 결과에 추가하고 리턴
+        if(target == 0){
+            // 이전 경로를 저장해둔 path를 결과에 삽입
             results.add(new ArrayList<>(path));
             return;
         }
-        
-        for(int i = start; i < candidates.length; i++) {
-            int c = candidates[i];
-            
-            // 원소의 합이 target보다 이미 큰 경우 탐색 미진행
-            if(sum + c > target) {
-                break;
-            }
-            
-            // 조합 목록에 c 추가
-            path.add(c);
-            
-            // dfs 재귀 호출
-            // 현재 탐색을 진행중인 노드의 인덱스 i를 전달한다. (자식 노드는 이 i부터 탐색을 시작)
-            dfs(results, path, candidates, sum + c, target, i);
-            
-            // c 제거
+
+        // 자기 자신보다 큰 숫자에 대해 dfs
+        for(int i = index; i < candidates.length; i++) {
+            path.add(candidates[i]);
+            dfs(results, candidates, target - candidates[i], i, path);
             path.removeLast();
         }
     }
     
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        // 결과 저장 리스트 선언
         List<List<Integer>> results = new ArrayList<>();
         
-        // 배열 정렬
-        Arrays.sort(candidates);
-        
-        // dfs 탐색 시작
-        dfs(results, new LinkedList<>(), candidates, 0, target, 0);
+        // dfs 탐색
+        dfs(results, candidates, target, 0, new ArrayDeque<>());
         
         return results;
     }
