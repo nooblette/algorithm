ee9bf4cf86f62b5beabda918b1ccc599a810ec3d
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-08-30 09:48:47 +0900

    Time: 2 ms (83.32%), Space: 44.9 MB (16.39%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,38 @@
+class Solution {
+    public void dfs(List<List<Integer>> results, List<Integer> path, int[] candidates, int sum, int target, int start) {
+        // 숫자 조합의 합이 target이 되는 경우 results에 추가
+        if(sum == target) {
+            results.add(new ArrayList<>(path));
+            return;
+        }
+        
+        for(int i = start; i < candidates.length; i++) {
+            int c = candidates[i];
+            
+            // 원소의 합이 target보다 이미 큰 경우 탐색 미진행
+            if(sum + c > target) {
+                break;
+            }
+            
+            // 조합 목록에 c 추가
+            path.add(c);
+            
+            // dfs 재귀 호출
+            dfs(results, path, candidates, sum + c, target, i);
+            
+            // c 제거
+            path.removeLast();
+        }
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> results = new ArrayList<>();
+        
+        // 배열 정렬
+        Arrays.sort(candidates);
+        
+        // dfs 탐색 시작
+        dfs(results, new LinkedList<>(), candidates, 0, target, 0);
+        
+        return results;
+    }
+}
