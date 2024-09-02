c61f7a8a338304175a08edcf9736c65480c05ac4
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-09-02 20:44:07 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.21 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,72 +1,54 @@
 import java.util.*;
 
 class Solution {
-    static class Bfs {
-        int level;
-        int y;
-        int x;
-        
-        public Bfs(int level, int y, int x) {
-            this.level = level;
+    // 우선순위 큐의 원소
+    static class Position {
+        final int y;
+        final int x;
+        final int distance;
+
+        public Position(int y, int x, int distance) {
             this.y = y;
             this.x = x;
+            this.distance = distance;
+        }
+    }
+
+    // Position의 거리(distance)를 기준으로 오름차순 정렬하는 우선순위 큐 선언
+    Queue<Position> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.distance)); 
+    
+    public void findPath(int y, int x, int distance, int[][] maps) {
+        // 게임 맵 내에 위치하면서
+        if(y >= 0 && y < maps.length && x >= 0 && x < maps[0].length && maps[y][x] != 0) {
+            // 이미 지나온 경로는 더 이상 거치지 않도록 기록
+            maps[y][x] = 0;
+
+            // 현재 위치와 거리 + 1을 우선순위 큐에 삽입
+            pq.add(new Position(y, x, distance + 1));
         }
     }
     
     public int solution(int[][] maps) {
-        Queue<Bfs> q = new LinkedList<>();
-        q.offer(new Bfs(0, 0, 0));
+        // 우선순위 큐에 출발지 삽입
+        pq.add(new Position(0, 0, 1));
         
-        boolean[][] visited = new boolean[maps.length][maps[0].length];
-        
-        // 모든 요소를 false로 초기화
-        for (int i = 0; i < maps.length; i++) {
-            Arrays.fill(visited[i], false);
+        while(!pq.isEmpty()) {
+            // 거리가 가장 짧은 원소 추출
+            Position cur = pq.poll();
+            
+            // 상대팀 진영에 도달했다면 탐색 종료
+            if(cur.y + 1 == maps.length && cur.x + 1 == maps[0].length) {
+                return cur.distance;
+            }
+                
+            // 동서남북 탐색
+            findPath(cur.y, cur.x + 1, cur.distance, maps); // 동
+            findPath(cur.y, cur.x - 1, cur.distance, maps); // 서
+            findPath(cur.y + 1, cur.x, cur.distance, maps); // 남
+            findPath(cur.y - 1, cur.x, cur.distance, maps); // 북
         }
-        visited[0][0] = true;
-
-        // bfs 탐색
-        int answer = 0;
-        while(true) {
-            if(q.isEmpty()) {
-                return -1;
-            }
             
-            Bfs bfs = q.poll();
-            
-            int y = bfs.y;
-            int x = bfs.x;
-            
-            if(y + 1 == maps.length && x + 1 == maps[0].length) {
-                return bfs.level + 1;
-            }
-            
-            int level = bfs.level + 1;
-            if(x+1 < maps[y].length && maps[y][x+1] == 1 && !visited[y][x+1]){
-                // 오른쪽 면이 존재하고 이동할 수 있고 방문한적이 없는 경우
-                visited[y][x+1] = true;
-                q.offer(new Bfs(level, y, x+1));
-            }
-                
-            if(x-1 >= 0 && maps[y][x-1] == 1 && !visited[y][x-1]) {
-                // 왼쪽 면이 존재하고 이동할 수 있고 방문한적이 없는 경우 경우
-                visited[y][x-1] = true;
-                q.offer(new Bfs(level, y, x-1));
-            }
-                
-            if(y+1 < maps.length && maps[y+1][x] == 1 && !visited[y+1][x]) {
-                // 아래쪽 면이 존재하고 이동할 수 있고 방문한적이 없는 경우
-                visited[y+1][x] = true;
-                q.offer(new Bfs(level, y+1, x));
-            }
-                
-            if(y-1 >= 0 && maps[y-1][x] == 1 && !visited[y-1][x]) {
-                // 위쪽 면이 존재하고 이동할 수 있고 방문한적이 없는 경우
-                visited[y-1][x] = true;
-                q.offer(new Bfs(level, y-1, x));
-            }
-            
-            answer += 1;
-        }
+        // 상대팀 진영에 도달할 수 없는 경우
+        return -1;
     }
 }
