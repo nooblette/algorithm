de7c7a859ac9302ca3ef9a40eddb4fb32103744b
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-09-05 22:25:24 +0900

    Time: 2 ms (92.55%), Space: 46.3 MB (96.14%) - LeetHub

--- 0687-longest-univalue-path/0687-longest-univalue-path.java
+++ 0687-longest-univalue-path/0687-longest-univalue-path.java
@@ -14,47 +14,35 @@
  * }
  */
 class Solution {
-    int result; 
+    int answer = 0;
     
-    private int dfs(TreeNode node) {
-        // 예외 처리 및 리프 노드까지 도달했을때
+    private int dfs(TreeNode node, int parentVal) {
+        // 리프 노드인 경우와 예외 처리
         if(node == null) {
+            // 동일한 값의 경로가 없으므로 0 반환
             return 0;
         }
         
-        // 왼쪽 자식에 대해 dfs
-        int left = dfs(node.left);
+        // 왼쪽 자식 노드 탐색, 현재 노드에서 왼쪽 자식 노드의 동일한 값 경로를 구한다.
+        int left = dfs(node.left, node.val);
         
-        // 현재 노드와 자식 노드의 값이 동일한 경우
-        if(node.left != null && node.left.val == node.val) {
-            left += 1;
-        } else {
-            // 현재 노드와 자식 노드의 값이 상이한 경우(동일한 값 경로가 깨지는 경우)
-            left = 0;
+        // 오른쪽 자식 노드 탐색, 현재 노드에서 오른쪽 자식 노드의 동일한 값 경로를 구한다.
+        int right = dfs(node.right, node.val);
+        
+        // 동일한 값 경로 갱신
+        answer = Math.max(answer, left + right);
+        
+        // 부모 노드의 값과 현재 노드의 값이 같다면 길이를 1 증가하여 반환
+        if(parentVal == node.val) {
+            return Math.max(left, right) + 1;
         }
         
-        // 오른쪽 자식에 대해 dfs
-        int right = dfs(node.right);
-        
-        // 현재 노드와 자식 노드의 값이 동일한 경우
-        if(node.right != null && node.right.val == node.val) {
-            right += 1;
-        } else {
-            // 현재 노드와 자식 노드의 값이 상이한 경우(동일한 값 경로가 깨지는 경우)
-            right = 0;
-        }      
-        
-        // 결과 값 갱신 - 동일한 값이 깨지는 경우 left 혹은 right를 0으로 초기화 하였다.
-        // 따라서 두 값의 합은 결국 가장 긴 경로의 길이가 된다.
-        result = Math.max(result, left + right);
-        
-        // 두 값 중 큰 값 반환, 반환 값은 부모 노드의 동일한 값 경로를 갱신하는데 다시 사용된다.
-        return Math.max(left, right);
+        // 부모 노드의 값과 현재 노드의 값이 다른 경우 - 동일한 값 경로가 깨지므로 0으로 초기화
+        return 0;
         
     }
-    
     public int longestUnivaluePath(TreeNode root) {
-        dfs(root);
-        return result;
+        dfs(root, -1);
+        return answer;
     }
 }
