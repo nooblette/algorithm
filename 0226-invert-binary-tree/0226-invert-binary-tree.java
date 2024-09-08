4eff36e6852ef79665e8c081082262d4ac9512a0
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-09-08 09:30:56 +0900

    Time: 0 ms (100.00%), Space: 41.3 MB (25.31%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -20,16 +20,17 @@ class Solution {
             return null;
         }
         
-        // 현재 노드의 값을 복사한 신규 노드 생성
-        TreeNode newNode = new TreeNode(root.val);
+        // 왼쪽/오른쪽 자식 노드 스왑
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
         
-        // 왼쪽 자식 노드는 현재 노드의 오른쪽 자식 노드에 대해 재귀 DFS 진행
-        newNode.left = invertTree(root.right);
+        // 왼쪽 자식 노드 dfs
+        invertTree(root.left);
         
-        // 오른쪽 자식 노드는 현재 노드의 왼쪽 자신 노드에 대해 재귀 DFS 진행
-        newNode.right = invertTree(root.left);
+        // 오른쪽 자식 노드 dfs
+        invertTree(root.right);
         
-        // 추가한 신규 노드 반환
-        return newNode;
+        return root;
     }
 }
