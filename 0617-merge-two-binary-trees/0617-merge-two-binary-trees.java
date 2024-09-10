0b5b1cb4760447e701eb9a7174456dd161819b48
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-09-10 22:13:47 +0900

    Time: 0 ms (100.00%), Space: 45 MB (36.59%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
@@ -14,28 +14,21 @@
  * }
  */
 class Solution {
-    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {        
-        // 탐색 종료
-        if(root1 == null && root2 == null) {
-            return null;
-        }
-        
-        // 두 트리 중 한 트리만 노드가 존재하는 경우
-        if(root1 == null) {
-            return root2;
-        }
-        
-        if(root2 == null) {
-            return root1;
-        }
-        
-        // 두 노드를 합산해야하는 경우
-        TreeNode merged = new TreeNode(root1.val + root2.val);
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        // 어느 한쪽이 null이면 null이 아닌 노드를 반환한다.
+        if(root1 == null) return root2;
+        if(root2 == null) return root1;
 
-        // 기존 자식과 연결
-        merged.left = mergeTrees(root1.left, root2.left);
-        merged.right = mergeTrees(root1.right, root2.right);
-        
-        return merged;
+        // 병합된 노드 생성
+        TreeNode node = new TreeNode(root1.val + root2.val);
+
+        // 병합된 노드의 왼쪽 자식 노드로 기존 노드 왼쪽 자식 노드 DFS 진행
+        node.left = mergeTrees(root1.left, root2.left);
+
+        // 병합된 노드의 오른쪽 자식 노드로 기존 노드 오른쪽 자식 노드 DFS 진행
+        node.right = mergeTrees(root1.right, root2.right);
+
+        // 병합된 노드 리턴
+        return node;
     }
 }
