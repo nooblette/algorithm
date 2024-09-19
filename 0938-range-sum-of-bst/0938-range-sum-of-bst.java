9a656e89b8bfea492d5e361506346d6360ea61d1
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-09-20 07:03:42 +0900

    Time: 2 ms (13.97%), Space: 50.9 MB (88.12%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.java
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -15,22 +15,36 @@
  */
 class Solution {
     public int rangeSumBST(TreeNode root, int low, int high) {
-        // 예외 처리
-        if(root == null) {
-            return 0;
+        // 반복 구조 DFS 구현을 위한 스택 선언
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        
+        // 루트 노드 스택에 삽입
+        stack.add(root);
+        
+        // 결과 변수 선언
+        int result = 0;
+        
+        // DFS 탐색
+        while(!stack.isEmpty()) {
+            TreeNode node = stack.remove();
+            
+            // 노드 값이 low보다 크면 왼쪽 자식 노드에 대해서 DFS 탐색을 위해 스택에 추가
+            if(node.val >= low && node.left != null) {
+                stack.add(node.left);
+            }
+            
+            // 노드 값이 high보다 작으면 오른쪽 자식 노드에 대해서 DFS 탐색을 위해 스택에 추가
+            if(node.val <= high && node.right != null) {
+                stack.add(node.right);
+            }
+            
+            // 노드의 값이 low와 high 사이에 속하면 결과에 누적
+            if(low <= node.val && node.val <= high) {
+                result += node.val;
+            }
         }
         
-        // 노드 값이 high보다 크면 왼쪽 서브트리만을 탐색
-        if(root.val > high) {
-            return rangeSumBST(root.left, low, high);
-        }
-
-        // 노드 값이 low보다 작으면 오른쪽 서브트리만을 탐색
-        if(root.val < low) {
-            return rangeSumBST(root.right, low, high);
-        }
+        return result;
         
-        // 위 두 if문을 통과하고 여기에 도달 했으면 현재 노드의 값이 low와 high 사이에 있으므로 결과 반환
-        return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
     }
 }
