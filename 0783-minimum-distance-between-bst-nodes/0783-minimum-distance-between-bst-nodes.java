1e493d08d98aef47d1d42bf4f1b0dee4aab84780
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-09-22 16:18:33 +0900

    Time: 0 ms (100.00%), Space: 41.5 MB (18.75%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.java
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.java
@@ -14,26 +14,40 @@
  * }
  */
 class Solution {
-    // 이전 노드 값, 오버 플로우를 방지하기 위해 10^5만큼 더하여 초기화
-    int prev = Integer.MIN_VALUE + 100000;
-    
-    // 노드 간 차이 최솟 값, 가장 큰 수로 설정
-    int minDiff = Integer.MAX_VALUE;
-    
     public int minDiffInBST(TreeNode root) {
-        // 중위 탐색
-        // 왼쪽 자식 노드 중 맨 아래 노드까지 탐색
-        if(root.left != null) minDiffInBST(root.left);
+        // 이전 노드 값, 오버 플로우를 방지하기 위해 10^5만큼 더하여 초기화
+        int prev = Integer.MIN_VALUE + 100000;
+
+        // 노드 간 차이 최솟 값, 가장 큰 수로 설정
+        int minDiff = Integer.MAX_VALUE;
         
-        // 현재 노드 값과 이전 노드 값의 차이를 구하고 최솟값이라면 갱신
-        // 항상 왼쪽 자식 -> 부모 -> 오른쪽 자식 순서로 방문하므로 root.val은 prev보다 크다.(차이로 음수가 나오는 경우가 없다.)
-        minDiff = Math.min(minDiff, root.val - prev);
+        // 반복 구조 DFS 구현을 위한 스택 선언
+        Deque<TreeNode> stack = new ArrayDeque<>();
         
-        // 현재 노드 값을 이전 노드 값으로 재설정
-        prev = root.val;
+        // 루트를 node 변수로 선언
+        TreeNode node = root;
         
-        // 오른쪽 자식 노드 재귀 탐색
-        if(root.right != null) minDiffInBST(root.right);
+        // DFS 탐색 - 스택이 비어있거나 node가 null이 될 때 까지 반복
+        while(!stack.isEmpty() || node != null) {
+            // 왼쪽 자식 노드의 리프 노드까지 스택에 삽입
+            while(node != null) {
+                stack.add(node);
+                node = node.left;
+            }
+            
+            // 스택에서 노드 추출
+            node = stack.removeLast();
+          
+            // 현재 노드 값과 이전 노드 값의 차이를 구하고 최솟값이라면 갱신
+            // 항상 왼쪽 자식 -> 부모 -> 오른쪽 자식 순서로 방문하므로 node.val은 prev보다 크다.(차이로 음수가 나오는 경우가 없다.)
+            minDiff = Math.min(minDiff, node.val - prev);
+            
+            // 현재 노드 값을 이전 노드 값으로 설정
+            prev = node.val;
+            
+            // 오른쪽 자식 노드로 지정하여 반복
+            node = node.right;
+        }
         
         return minDiff;
     }
