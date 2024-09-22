50c4c582ddccc52bd2f294a841f7e5a3c774fb5f
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-09-22 16:09:01 +0900

    Time: 0 ms (100.00%), Space: 41.2 MB (39.22%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.java
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.java
@@ -14,30 +14,27 @@
  * }
  */
 class Solution {
-    int result = Integer.MAX_VALUE;
+    // 이전 노드 값, 오버 플로우를 방지하기 위해 10^5만큼 더하여 초기화
+    int prev = Integer.MIN_VALUE + 100000;
     
-    // 트리를 dfs 탐색
-    public void dfs(TreeNode node, List<TreeNode> visited) {
-        // 예외 처리
-        if(node == null) {
-            return;
-        }
-        
-        for(TreeNode v : visited) {
-            // 최솟값 갱신
-            if(Math.abs(v.val - node.val) < result) {
-                result = Math.abs(v.val - node.val);
-            }
-        }
-        
-        visited.add(node);
-        dfs(node.left, visited);
-        dfs(node.right, visited);
-        visited.remove(node);
-    }
+    // 노드 간 차이 최솟 값, 가장 큰 수로 설정
+    int minDiff = Integer.MAX_VALUE;
     
     public int minDiffInBST(TreeNode root) {
-        dfs(root, new ArrayList<>());
-        return result;
+        // 중위 탐색
+        // 왼쪽 자식 노드 중 맨 아래 노드까지 탐색
+        if(root.left != null) minDiffInBST(root.left);
+        
+        // 현재 노드 값과 이전 노드 값의 차이를 구하고 최솟값이라면 갱신
+        // 항상 왼쪽 자식 -> 부모 -> 오른쪽 자식 순서로 방문하므로 root.val은 prev보다 크다.(차이로 음수가 나오는 경우가 없다.)
+        minDiff = Math.min(minDiff, root.val - prev);
+        
+        // 현재 노드 값을 이전 노드 값으로 재설정
+        prev = root.val;
+        
+        // 오른쪽 자식 노드 재귀 탐색
+        if(root.right != null) minDiffInBST(root.right);
+        
+        return minDiff;
     }
 }
