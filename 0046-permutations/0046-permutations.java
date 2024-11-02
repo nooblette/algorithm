dd94e73878f43c51c25cda41457446e1d30d90cc
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-11-02 21:08:26 +0900

    Time: 2 ms (43.97%), Space: 44.2 MB (80.99%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,44 +1,31 @@
-class Solution {
-        public void dfs(
-        List<List<Integer>> results, 
-        List<Integer> prevElements, 
-        List<Integer> elements
-    ) {
-        // 리프노드에 도달하면 결과에 추가
-        if(elements.isEmpty()) {
-            results.add(prevElements.stream().collect(Collectors.toList()));
+class Solution {    
+    private void dfs(Integer num, int[] nums, List<Integer> path, List<List<Integer>> answer) {
+        
+        // 이미 결과를 찾는데 사용한 숫자인 경우
+        if(path.contains(num)) {
+            return;
         }
-
-        // 전달받은 elements 탐색
-        for(Integer e : elements) {
-            // 전달받은 엘리먼트에서 현재 엘리먼트를 제외하고 nextElements를 새롭게 구성
-            // elements를 바로 nextElements에 할당하면 동일한 참조값을 가리키게되어 별도로 리스트를 선언한다.
-            List<Integer> nextElements = new ArrayList<>(elements);
-            nextElements.remove(e);
-
-            // prevElements 기존 값에 현재 엘리먼트 추가(방문한 경로를 저장하는 역할)
-            prevElements.add(e);
-
-            // dfs g호출
-            dfs(results, prevElements, nextElements);
-
-            // 돌아온 이후 prevElements 현재 엘리먼트 삭제
-            prevElements.remove(e);
+        
+        // DFS 탐색 깊이가 nums의 길이와 동일한 경우
+        path.add(num);
+        if(path.size() == nums.length) {
+            answer.add(new ArrayList<>(path));
+            path.remove(num);
+            return;
         }
+        
+        for(int newNum : nums) {
+            dfs(newNum, nums, path, answer);
+        }
+        path.remove(num);
     }
     
     public List<List<Integer>> permute(int[] nums) {
-        // 결과 저장 리스트 선언
-        List<List<Integer>> results = new ArrayList<>();
+        List<List<Integer>> answer = new ArrayList<>();
+        for(int num : nums) {
+            dfs(num, nums, new ArrayList<>(), answer);
+        }
         
-        // int[]를 List<Integer>로 변경
-        List<Integer> lst = Arrays.stream(nums)
-                .boxed()
-                .collect(Collectors.toList());
-        
-        // dfs 시작
-        dfs(results, new ArrayList<>(), lst);
-        
-        return results;
+        return answer;
     }
 }
