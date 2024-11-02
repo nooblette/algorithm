af1d7066de320aa706147de4df834c86a1356696
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-11-02 22:08:18 +0900

    Time: 18 ms (61.40%), Space: 43.5 MB (91.37%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,23 +1,28 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         // 중복 방지를 위해 Set으로 선언
-        Set<List<Integer>> answer = new HashSet<>();
+        List<List<Integer>> answer = new ArrayList<>();
         
         // 주어진 배열을 정렬
         Arrays.sort(nums);
         
         // i번째 인덱스와 i+1번째 인덱스는 고정
         for(int i = 0; i < nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // 중복 i 건너뛰기
             for(int j = i + 1; j < nums.length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue; // 중복 j 건너뛰기
+                
                 // 이 후 원소에 대해 포인터를 옮겨가며 탐색
                 int left = j + 1;
                 int right = nums.length - 1;
                 
                 while(left < right && right < nums.length) {
+                    
                     // 4개의 원소의 합이 target 보다 작은 경우
                     if((long) nums[i] + nums[j] + nums[left] + nums[right] < target) {
                         // left를 한 칸 우측으로 이동한다 (합이 증가하는 방향)
                         left += 1;
+                    
                     } else if((long) nums[i] + nums[j] + nums[left] + nums[right] > target) {
                         // 4개의 원소의 합이 target 보다 큰 경우
                         // right를 한 칸 좌측으로 이동한다 (합이 감소하는 방향)
@@ -25,10 +30,14 @@ class Solution {
                     } else {
                         // 4개의 원소의 합이 target이 되는 경우
                         answer.add(List.of(nums[i], nums[j], nums[left], nums[right]));
+                                                
+                        // left와 right 포인터에서 중복된 값을 건너뛰어 중복 결과를 방지한다.
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
                         
-                        // left는 우측으로, right는 좌측으로 이동 (두 포인터를 모두 이동)
-                        left += 1;
-                        right -= 1;
+                        // left와 right를 각각 한 칸씩 이동
+                        left++;
+                        right--;
                     } 
                 }
                 
