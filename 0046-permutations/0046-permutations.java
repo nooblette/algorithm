9d554a3ef0250ddf115c621c45d58ca17b6fd94d
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-11-02 21:14:58 +0900

    Time: 1 ms (94.14%), Space: 44.1 MB (90.59%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,30 +1,31 @@
-class Solution {    
-    private void dfs(Integer num, int[] nums, List<Integer> path, List<List<Integer>> answer) {
-        
-        // 이미 결과를 찾는데 사용한 숫자인 경우
-        if(path.contains(num)) {
-            return;
-        }
-        
-        // DFS 탐색 깊이가 nums의 길이와 동일한 경우
-        path.add(num);
+class Solution {
+    private void dfs(List<Integer> path, List<List<Integer>> answer, int[] nums) {
+        // 모든 숫자를 방문한 경우
         if(path.size() == nums.length) {
             answer.add(new ArrayList<>(path));
-            path.remove(num);
             return;
         }
         
-        for(int newNum : nums) {
-            dfs(newNum, nums, path, answer);
+        // 각 원소에 대해 dfs 탐색 진행
+        for(Integer num : nums) {
+            // 이미 방문한 노드라면 다음 원소에 대해 탐색
+            if(path.contains(num)) {
+                continue;
+            }
+            
+            path.add(num);
+            dfs(path, answer, nums);
+            path.remove(num);
         }
-        path.remove(num);
     }
     
     public List<List<Integer>> permute(int[] nums) {
+        // 결과를 담을 변수
         List<List<Integer>> answer = new ArrayList<>();
-        for(int num : nums) {
-            dfs(num, nums, new ArrayList<>(), answer);
-        }
+        
+        // 방문한 경로
+        List<Integer> path = new ArrayList<>();
+        dfs(path, answer, nums);
         
         return answer;
     }
