68bffebb334b0fbd9539a4a191b48b2d73f6a8ab
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-11-03 20:09:42 +0900

    Time: 15 ms (87.29%), Space: 41.9 MB (91.05%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,30 +1,40 @@
 class Solution {
-    private boolean isPalindrome(int left, int right, String s) {
-        while(left < right && s.charAt(left) == s.charAt(right)) {
-            left += 1;
-            right -= 1;
+    private int expandAroundCenter(String s, int left, int right) {
+        // left와 right를 중심으로 확장해가며 최장 길이 팰린드롬 탐색
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left -= 1;
+            right += 1;
         }
         
-        return left >= right;
+        // 팰린드롬이 아닌 경우 while문을 빠져나오므로 팰린드롬 길이를 구하기 위해서 -1을 해주어야한다.
+        // right - 1 - (left + 1) + 1 = right - left - 1
+        return right - left - 1;
     }
     
     public String longestPalindrome(String s) {
-        String answer = "";
-        int len = 0;
+        int start = 0;
+        int end = 0;
         
         for(int i = 0; i < s.length(); i++) {
-            for(int j = i; j < s.length(); j++) {
-                // 두 문자가 동일하다면, 둘 사이 문자에 대해 팰린드롬이 맞는지 체크
-                if(s.charAt(i) == s.charAt(j) && isPalindrome(i, j, s)) {
-                    // 팰린드롬이 맞고 더 길이가 길다면 갱신
-                    if(len < j - i + 1) {
-                        answer = s.substring(i, j + 1);
-                        len = j - i + 1;
-                    }
-                }
+            // i를 중심으로 확장해가며 최장길이 팰린드롬 탐색
+            int lenOdd = expandAroundCenter(s, i, i); // 길이가 홀수인 팰린드롬
+            int lenEven = expandAroundCenter(s, i, i+1); // 길이가 짝수인 팰린드롬
+            
+            // i가 중심인 경우 등장하는 팰린드롬 중 최대 길이 반환
+            int len = Math.max(lenOdd, lenEven);
+            
+            // 최장 길이를 갱신하는 경우
+            if(len > end - start) {
+                // i를 중심으로 (len-1)/2만큼 좌측으로 이동하면 시작 지점
+                // 팰린드롬의 길이가 짝수인 경우를 위해 한쪽으로 쏠리므로 -1을 빼준다.
+                start = i - (len-1)/2;
+                
+                // i를 중심으로 len/2만큼 우측으로 이동하면 종료 지점
+                end = i + len/2;
             }
+            
         }
         
-        return answer;
+        return s.substring(start, end + 1);
     }
 }
