410ce0591d50fcc32283e8c72f79dce554aba535
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-11-03 20:31:46 +0900

    Time: 115 ms (39.75%), Space: 46.6 MB (9.73%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,40 +1,46 @@
-class Solution {
-    private int expandAroundCenter(String s, int left, int right) {
-        // left와 right를 중심으로 확장해가며 최장 길이 팰린드롬 탐색
-        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left -= 1;
-            right += 1;
-        }
-        
-        // 팰린드롬이 아닌 경우 while문을 빠져나오므로 팰린드롬 길이를 구하기 위해서 -1을 해주어야한다.
-        // right - 1 - (left + 1) + 1 = right - left - 1
-        return right - left - 1;
-    }
-    
+class Solution { 
     public String longestPalindrome(String s) {
-        int start = 0;
-        int end = 0;
+        int n = s.length();
+        if (n <= 1) return s;
+
+        // `dp[i][j]`는 부분 문자열 `s[i..j]`가 팰린드롬인지 여부를 저장한다.
+        boolean[][] dp = new boolean[n][n];
         
-        for(int i = 0; i < s.length(); i++) {
-            // i를 중심으로 확장해가며 최장길이 팰린드롬 탐색
-            int lenOdd = expandAroundCenter(s, i, i); // 길이가 홀수인 팰린드롬
-            int lenEven = expandAroundCenter(s, i, i+1); // 길이가 짝수인 팰린드롬
-            
-            // i가 중심인 경우 등장하는 팰린드롬 중 최대 길이 반환
-            int len = Math.max(lenOdd, lenEven);
-            
-            // 최장 길이를 갱신하는 경우
-            if(len > end - start + 1) {
-                // i를 중심으로 (len-1)/2만큼 좌측으로 이동하면 시작 지점
-                // 팰린드롬의 길이가 짝수인 경우를 위해 한쪽으로 쏠리므로 -1을 빼준다.
-                start = i - (len-1)/2;
-                
-                // i를 중심으로 len/2만큼 우측으로 이동하면 종료 지점
-                end = i + len/2;
-            }
-            
+        int maxLen = 1; // 최소 길이 1의 팰린드롬 고려
+        int start = 0;
+        
+        // 길이 1인 부분 문자열은 모두 팰린드롬
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
         }
         
-        return s.substring(start, end + 1);
+        // 길이 2인 부분 문자열 초기화
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                start = i;
+                maxLen = 2;
+            }
+        }
+        
+        // 길이 3 이상인 부분 문자열 탐색
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+
+                // 두 문자가 같고, 내부 문자열이 팰린드롬인 경우
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+
+                    // 가장 긴 팰린드롬 갱신
+                    if (len > maxLen) {
+                        maxLen = len;
+                        start = i;
+                    }
+                }
+            }
+        }
+
+        return s.substring(start, start + maxLen);
     }
 }
