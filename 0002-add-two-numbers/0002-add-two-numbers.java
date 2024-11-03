2a023167a6ae06130bfb1259e52fbd536c0d2477
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-11-03 17:23:25 +0900

    Time: 1 ms (100.00%), Space: 44.2 MB (82.65%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,44 +10,42 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        // 값을 계산할 임시 노드 선언
-        ListNode node = new ListNode(0);
+        // 합한 결과 저장을 연결하기 위해 선언
+        ListNode root = new ListNode();
         
-        // 임시 노드를 첫번째 노드로 선언
-        ListNode root = node;
+        // 정답 반환을 위해 선언
+        ListNode answer = root;
         
-        // 자릿수의 합(sum), 자리올림수(carry), 나머지(remainder)를 저장할 변수 선언
-        int sum, carry = 0, remainder = 0;
-        
-        // 두 연결리스트 l1과 l2를 끝까지 순회, 자리 올림수도 0이 될때까지 진행
-        while(l1 != null || l2 != null || carry != 0) {
-            sum = 0;
+        int carry = 0;
+        while(!(l1 == null && l2 == null)) {
+            int sum = carry + (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0);
             
-            // 첫 번째 연결리스트 합산 및 진행
-            if(l1 != null) {
-                sum += l1.val;
-                l1 = l1.next;
+            // carry 초기화
+            carry = 0;
+            
+            // 합한 결과가 10보다 큰 경우
+            if(sum >= 10) {
+                carry = 1;
+                sum -= 10;
             }
             
-            // 두 번째 연결리스트 합산 및 진행
-            if(l2 != null) {
-                sum += l2.val;
-                l2 = l2.next; 
-            }
+            // 정답 리스트에 추가
+            root.next = new ListNode(sum);
             
-            // 자리올림수와 나머지 계산
-            remainder = (sum + carry) % 10;
-            carry = (sum + carry) / 10;
+            // 다음 노드로 탐색 진행
+            if(l1 != null) l1 = l1.next;
             
-            // 나머지 값을 현재 노드의 다음 노드로 지정
-            node.next = new ListNode(remainder);
+            // 다음 노드로 탐색 진행
+            if(l2 != null) l2 = l2.next;
             
-            // 다음 노드로 이동
-            node = node.next;
+            // 다음 노드로 변경
+            root = root.next;
         }
         
-        // 첫번째 노드(root)는 임시노드, 따라서 다음 노드를 결과로 반환
-        return root.next;
+        // 만약 carry가 남아있다면 추가
+        if(carry > 0) root.next = new ListNode(carry);
         
+        // 정답 리스트에 추가했으므로 첫번째 원소의 다음 노드를 반환
+        return answer.next;
     }
 }
