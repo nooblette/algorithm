3820e0f1dced92c16aafe2612754a6c9724fe29d
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-11-04 15:27:53 +0900

    Time: 41 ms (10.21%), Space: 94.8 MB (15.94%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,32 +1,21 @@
 class Solution {
-    private void dfs(int[] nums, List<Integer> path, List<List<Integer>> answer, int k, int start) {
+    private void dfs(List<Integer> path, List<List<Integer>> answer, int n, int k, int start) {
         if(path.size() == k) {
             answer.add(new ArrayList<>(path));
             return;
         }
         
-        for(int num : nums) {
-            if(num > start) {
-                path.add(num);
-                dfs(nums, path, answer, k, num);
-                path.remove(Integer.valueOf(num));
-            }
+        for(int i = start; i < n; i++) {
+            path.add(i + 1);
+            dfs(path, answer, n, k, i + 1);
+            path.remove(Integer.valueOf(i + 1));
         }
-        
     }
     
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> answer = new ArrayList<>();
         
-        // 1부터 n까지 배열 선언
-        int[] nums = new int[n];
-        
-        // 초기화
-        for(int i = 0; i < n; i++) {
-            nums[i] = i + 1;
-        }
-        
-        dfs(nums, new ArrayList<>(), answer, k, 0);
+        dfs(new ArrayList<>(), answer, n, k, 0);
         return answer;
     }
 }
