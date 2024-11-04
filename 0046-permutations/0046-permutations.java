e10ce8e44b65b728e22bb4a425032d81960f9b46
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-11-04 15:13:03 +0900

    Time: 2 ms (44.47%), Space: 44.4 MB (66.21%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,31 +1,23 @@
 class Solution {
-    private void dfs(List<Integer> path, List<List<Integer>> answer, int[] nums) {
-        // 모든 숫자를 방문한 경우
+    private void dfs(int[] nums, List<Integer> path, List<List<Integer>> answer) {
         if(path.size() == nums.length) {
             answer.add(new ArrayList<>(path));
             return;
         }
         
-        // 각 원소에 대해 dfs 탐색 진행
-        for(Integer num : nums) {
-            // 이미 방문한 노드라면 다음 원소에 대해 탐색
-            if(path.contains(num)) {
-                continue;
+        for(int num : nums) {
+            if(!path.contains(num)) {
+                path.add(num);
+                dfs(nums, path, answer);
+                path.remove(Integer.valueOf(num));
             }
-            
-            path.add(num);
-            dfs(path, answer, nums);
-            path.remove(num);
         }
     }
     
     public List<List<Integer>> permute(int[] nums) {
-        // 결과를 담을 변수
         List<List<Integer>> answer = new ArrayList<>();
         
-        // 방문한 경로
-        List<Integer> path = new ArrayList<>();
-        dfs(path, answer, nums);
+        dfs(nums, new ArrayList<>(), answer);
         
         return answer;
     }
