12bb6e1f67f18496c634d0596f6bb764e94689b5
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-11-04 15:20:34 +0900

    Time: 101 ms (5.03%), Space: 94.3 MB (73.29%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,37 +1,36 @@
 class Solution {
-    public void dfs(
-        List<List<Integer>> results,
-        List<Integer> elements,
-        int n,
-        int k,
-        int start // start는 이전 레벨의 탐색보다 항상 1만큼 큰 값을 갖는다.
-    ) {
-        // k개의 조합을 모두 탐색한 경우
-        if(k == 0) {
-            results.add(new ArrayList<>(elements));
+    private void dfs(int[] nums, List<Integer> path, List<List<Integer>> answer, int k, int start) {
+        if(path.size() == k) {
+            answer.add(new ArrayList<>(path));
             return;
         }
-
-        // 현재 엘리먼트 이후 인덱스부터 이후 엘리먼트 탐색
-        for(int i = start; i <= n; i++) {
-            // 전달받은 엘리먼트에 현재 엘리먼트 추가
-            elements.add(i);
-
-            // 재귀 dfs
-            dfs(results, elements, n, k - 1, i + 1);
-
-            // 돌아온 이후 현재 엘리먼트 제거
-            elements.removeLast();
+        
+        for(int num : nums) {
+            if(num < start) {
+                continue;
+            }
+            
+            if(!path.contains(num)) {
+                path.add(num);
+                dfs(nums, path, answer, k, num);
+                path.remove(Integer.valueOf(num));
+            }
         }
+        
     }
-        
+    
     public List<List<Integer>> combine(int n, int k) {
-        // 결과 저장 리스트 선언
-        List<List<Integer>> results = new ArrayList();
+        List<List<Integer>> answer = new ArrayList<>();
         
-        // dfs 탐색 시작
-        dfs(results, new LinkedList<>(), n, k, 1);
+        // 1부터 n까지 배열 선언
+        int[] nums = new int[n];
         
-        return results;
+        // 초기화
+        for(int i = 0; i < n; i++) {
+            nums[i] = i + 1;
+        }
+        
+        dfs(nums, new ArrayList<>(), answer, k, 0);
+        return answer;
     }
 }
