d639b4fbd4e8b488ef56fa309ea591c451e5e544
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-11-04 16:09:50 +0900

    Time: 4 ms (16.10%), Space: 44.1 MB (95.61%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,32 +1,27 @@
 class Solution {
-    public void dfs(List<List<Integer>> results, int[] candidates, int target, int index, Deque<Integer> path) {
-        // 0보다 작다면 목표값을 넘었으므로 리턴
+    private void dfs(int[] candidates, List<Integer> path, int target, List<List<Integer>> answer, int start) {
+        // 만약 target이 0보다 작다면 더 이상 합이 target이 되는 조합을 찾을 수 없으므로 탐색 종료
         if(target < 0) {
             return;
         }
-
-        // 0이면 목표값에 도달했으므로 결과에 추가하고 리턴
-        if(target == 0){
-            // 이전 경로를 저장해둔 path를 결과에 삽입
-            results.add(new ArrayList<>(path));
+        
+        // 정답 조합을 찾은 경우
+        if(target == 0) {
+            answer.add(new ArrayList<>(path));
             return;
         }
-
-        // 자기 자신보다 큰 숫자에 대해 dfs
-        for(int i = index; i < candidates.length; i++) {
+        
+        for(int i = start; i < candidates.length; i++) {
             path.add(candidates[i]);
-            dfs(results, candidates, target - candidates[i], i, path);
-            path.removeLast();
+            dfs(candidates, path, target - candidates[i], answer, i);
+            path.remove(Integer.valueOf(candidates[i]));
         }
     }
     
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        // 결과 저장 리스트 선언
-        List<List<Integer>> results = new ArrayList<>();
+        List<List<Integer>> answer = new ArrayList<>();
         
-        // dfs 탐색
-        dfs(results, candidates, target, 0, new ArrayDeque<>());
-        
-        return results;
+        dfs(candidates, new ArrayList<>(), target, answer, 0);
+        return answer;
     }
 }
