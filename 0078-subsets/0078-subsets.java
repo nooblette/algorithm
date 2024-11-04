ff9241978ab6d97c8cfb8d94ceb7cf0c4583bc16
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-11-04 16:15:30 +0900

    Time: 1 ms (54.63%), Space: 42.8 MB (21.35%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,28 +1,24 @@
 class Solution {
-    public void dfs(List<List<Integer>> results, int[] nums, int index, Deque<Integer> path) {
-        // 모든 탐색 경로를 매번 결과에 추가
-        results.add(new ArrayList<>(path));
-
-        // 자기자신보다 큰 숫자를 dfs 진행
-        for(int i = index; i < nums.length; i++) {
-            // path에 현재 엘리먼트 추가
-            path.add(nums[i]);
-
-            // 재귀 dfs
-            dfs(results, nums, i + 1, path);
-
-            // 돌아온 이후 현재 엘리먼트 삭제
-            path.removeLast();
+    private void dfs(int[] nums, List<Integer> path, List<List<Integer>> answer, int start) {
+        answer.add(new ArrayList<>(path));
+        
+        for(int i = start; i < nums.length; i++) {
+            int num = nums[i];
+            
+            if(!path.contains(num)) {
+                path.add(num);
+                dfs(nums, path, answer, i);
+                path.removeLast();
+            }
         }
+        
     }
-
+    
     public List<List<Integer>> subsets(int[] nums) {
-        // 결과 저장 리스트
-        List<List<Integer>> results = new ArrayList<>();
+        List<List<Integer>> answer = new ArrayList<>();
         
-        // dfs 시작
-        dfs(results, nums, 0, new ArrayDeque());
+        dfs(nums, new ArrayList<>(), answer, 0);
         
-        return results;
+        return answer;
     }
 }
