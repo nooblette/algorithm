edb8a21a3362708967bd423292d4f8deb94a24ff
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-11-07 21:22:21 +0900

    Time: 20 ms (59.05%), Space: 58.1 MB (63.35%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -8,13 +8,10 @@ class Solution {
             minDay = Math.min(minDay, day);
         }
         
-        // 최대 일자 저장
-        final int finalMaxDay = maxDay;
-        
         // 부케를 만들 수 없는 경우 -1을 반환한다.
         if(bloomDay.length < m * k) return -1;
         
-        int answer = maxDay + 1;
+        int answer = Integer.MAX_VALUE;
 	    while(minDay <= maxDay) {
 		    // 주어진 날짜 범위에 대해 이분탐색 진행
 		    int mid = minDay + (maxDay - minDay) / 2;
@@ -26,19 +23,18 @@ class Solution {
             int bouquet = 0;
             
             for(int day : bloomDay) {
-                // 꽃이 필 수 없는 경우 넘어간다.
-                if(mid < day) {
-                    // 연속이 끊겼으므로 k로 다시 초기화
-                    count = k;
-                    continue;
-                }
                 // 꽃이 필 수 있는 경우
-                count--;
-                
-                // k개의 연속된 꽃이 모두 핀 경우
-                if(count == 0) {
+                if(day <= mid) {
+                    count--;
+                    
+                    // k개의 연속된 꽃이 모두 핀 경우
+                    if(count == 0) {
+                        count = k;
+                        bouquet += 1;
+                    }
+                } else {
+                    // 꽃이 필 수 없는 경우 연속이 끊겼으므로 k로 다시 초기화 한다.
                     count = k;
-                    bouquet += 1;
                 }
             }
             
@@ -52,7 +48,7 @@ class Solution {
             }
         }
         
-        // 최대 일자보다 커진 경우 부케를 m개 만들 수 없으므로 -1을 반환한다.
-        return answer > finalMaxDay ? -1 : answer;
+        // 최대 일자보다 커진 경우 부케를 m -1을 반환한다.
+        return answer == Integer.MAX_VALUE ? -1 : answer;
     }
 }
