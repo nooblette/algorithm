9c2969b084d956cf1f55747e9274cd4306f64d75
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2024-11-09 17:22:49 +0900

    Time: 25 ms (5.75%), Space: 44.4 MB (44.96%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,37 +14,39 @@
  * }
  */
 class Solution {
-    public TreeNode dfs(
-        int preIndex, // 전위 순회 배열의 인데스(preorder[preIndex] 값이 현재 탐색중인 트리의 최상위 노드가 된다.)
-        int inStart, int inEnd, int[] preorder, int[] inorder) {
+    private TreeNode dfs(List<Integer> preOrder, List<Integer> inOrder) {
         // 예외 처리
-        if(preIndex > preorder.length - 1 || inStart > inEnd) {
+        if(inOrder.isEmpty()) {
             return null;
         }
         
         // 전위 순회 값이 중위 순회에서는 몇번째 인덱스인지 추출
-        int inIndex = 0;
-        for(int i = inStart; i <= inEnd; i++) {
-            if(inorder[i] == preorder[preIndex]) {
-                inIndex = i;
-            }
-        }
+        int inIndex = inOrder.indexOf(preOrder.get(0));
         
-        // 전위 순회 값을 갖는 inOrder 배열의 인덱스는 중위 순회 배열을 분할하는 기준 노드가 된다.
-        TreeNode node = new TreeNode(inorder[inIndex]);
+        // 해당 인덱스는 중위 순회를 분할하는 노드로 지정
+        TreeNode node = new TreeNode(inOrder.get(inIndex));
         
-        // 전위 순회 다음 결과를 보도록 1 증가
-        preIndex++;
+        // 왼쪽 서브트리에 대해 반복 - preOrder[1..inIndex], inOder[0..inIndex - 1]
+        node.left = dfs(preOrder.subList(1, inIndex + 1), inOrder.subList(0, inIndex));
         
-        // 전위 순회 값을 갖는 inOrder 배열의 인덱스를 기준으로 왼쪽은 왼쪽 서브트리, 오른쪽은 오른쪽 서브트리가 된다.
-        node.left = dfs(preIndex, inStart, inIndex - 1, preorder, inorder);
-        node.right = dfs(preIndex + inIndex - inStart, inIndex + 1, inEnd, preorder, inorder);
+        // 오른쪽 서브트리에 대해 반복
+        node.right = dfs(preOrder.subList(inIndex + 1, preOrder.size()), inOrder.subList(inIndex + 1, inOrder.size()));
         
         return node;
     }
     
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        // 재구 DFS 진행, 초기 값은 전위 순회 인덱스: 0, 중위 순회 시작 인덱스: 0, 종료: 길이 - 1
-        return dfs(0, 0, inorder.length - 1, preorder, inorder);
+        // 원시형을 참조형으로 처리하기 위해 변수 선언
+        List<Integer> preOrder = Arrays.stream(preorder)
+                .boxed()
+                .collect(Collectors.toList());
+        
+        List<Integer> inOrder = Arrays.stream(inorder)
+                .boxed()
+                .collect(Collectors.toList());
+
+        
+        // 재귀 DFS 진행, 리스트로 처리하면 파라미터가 간결해진다.
+        return dfs(preOrder, inOrder);
     }
 }
