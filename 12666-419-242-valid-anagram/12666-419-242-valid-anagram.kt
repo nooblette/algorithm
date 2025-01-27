b19932d6088348c7065d8322416a0ca6445b4c33
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-01-27 11:53:11 +0900

    Time: 12 ms, Memory: 39.6 MB - LeetHub

--- 12666-419-242-valid-anagram/12666-419-242-valid-anagram.kt
+++ 12666-419-242-valid-anagram/12666-419-242-valid-anagram.kt
@@ -1,10 +1,3 @@
 class Solution {
-    fun sort(s: String): String {
-        val c = s.toCharArray()
-        Arrays.sort(c)
-        
-        return String(c)
-    }
-    
-    fun isAnagram(s: String, t: String): Boolean = sort(s) == sort(t)
+    fun isAnagram(s: String, t: String): Boolean = String(s.toCharArray().apply { sort() }) == String(t.toCharArray().apply { sort() })
 }
