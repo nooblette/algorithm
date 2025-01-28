feecfe661fa787666ed0faf00fccc0237d94bfba
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-01-28 10:23:10 +0900

    Time: 3 ms, Memory: 46.9 MB - LeetHub

--- 12234-1453-167-two-sum-ii-input-array-is-sorted/12234-1453-167-two-sum-ii-input-array-is-sorted.java
+++ 12234-1453-167-two-sum-ii-input-array-is-sorted/12234-1453-167-two-sum-ii-input-array-is-sorted.java
@@ -1,22 +1,15 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        // 왼쪽 포인터는 가장 좌측 지정
-        int left = 0;
-
-        // 오른쪽 포인터는 가장 우측 지정
-        int right = numbers.length - 1;
-
-        // 왼쪽 포인터가 오른쪽 포인터보다 작다면 계속 탐색
-        while(left < right) {
-            // 합이 target보다 작으면 왼쪽 포인터를 한 칸 증가
-            if(numbers[left] + numbers[right] < target) {
-                left++;
-            } else if(numbers[left] + numbers[right] > target) {
-                // 합이 target보다 크다면 오른쪽 포인터를 한 칸 감소
-                right--;
-            } else {
-                // 합이 target과 같다면 정답을 찾았으니 반환
-                return new int[]{left + 1, right + 1};
+        // 배열 순회
+        for(int i = 0; i < numbers.length; i++) {
+            // target에서 현재 순회중인 원소의 값을 뺀다
+            int expected = target - numbers[i];
+            
+            // 이진 검색으로 expected가 numbers 배열에 있는지 탐색
+            int idx = Arrays.binarySearch(numbers, i + 1, numbers.length, expected);
+            // 있다면 결과로 반환
+            if(idx >= 0) {
+                return new int[]{i + 1, idx + 1};
             }
         }
         
