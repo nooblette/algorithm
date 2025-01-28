b0c5b0ef75674c0b6ddce1e263173ec0e48f6a05
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-01-28 09:52:45 +0900

    Time: 5 ms, Memory: 43.2 MB - LeetHub

--- 6306-2313-349-intersection-of-two-arrays/6306-2313-349-intersection-of-two-arrays.java
+++ 6306-2313-349-intersection-of-two-arrays/6306-2313-349-intersection-of-two-arrays.java
@@ -3,15 +3,30 @@ class Solution {
         // 결과 변수로 중복을 허용하지 않는 집합 자료형 선언
         Set<Integer> result = new HashSet<>();
         
-        // 이진 검색으로 탐색
+        // 두 배열 정렬
+        Arrays.sort(nums1);
         Arrays.sort(nums2);
-        for(int num1 : nums1) {
-            if(Arrays.binarySearch(nums2, num1) >= 0) {
-                result.add(num1);
+        
+        // 투 포인터 선언
+        int i = 0;
+        int j = 0;
+        
+        while(i < nums1.length && j < nums2.length) {
+            // 값이 작은 쪽의 포인터가 앞으로 이동
+            if(nums1[i] < nums2[j]) {
+                i++;
+            } else if(nums2[j] < nums1[i]) {
+                j++;
+            } else {
+                // 값이 일치하는 경우 결과 삽입 후 포인터 모두 이동
+                result.add(nums1[i]);
+                i++;
+                j++;
             }
         }
         
-        // HashSet()을 int[]로 변환하여 반환
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        return result.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
     }
 }
