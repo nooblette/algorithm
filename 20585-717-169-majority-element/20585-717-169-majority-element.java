e2ceb704bc761baeb6ce8c84c9d82d4ece694a69
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-03-22 19:14:36 +0900

    Time: 3 ms, Memory: 53.1 MB - LeetHub

--- 20585-717-169-majority-element/20585-717-169-majority-element.java
+++ 20585-717-169-majority-element/20585-717-169-majority-element.java
@@ -1,28 +1,29 @@
 class Solution {
-    public int majorityElement(int[] nums) {
-        // 엘리먼트 개수 계산
-        Map<Integer, Integer> countsMap = new HashMap<>();
-        for(int num : nums) {
-            countsMap.put(num, countsMap.getOrDefault(num, 0) + 1);
+    public int majorityElement(int left, int right, int[] nums) {
+        // 최소 분할 단위 리턴
+        if(left == right) {
+            return nums[left];
         }
-
-        // countsMap을 내림차순 정렬하여 Map에 저장한다.
-        // 여기서는 첫번째 항목을 구해야하므로 입력 순서가 유지되는 LinkedHashMap으로 구현한다.
-        // 참고로 HashMap은 입력 순서를 유지하지 않는다.
-        Map<Integer, Integer> reverseSortedMap = new LinkedHashMap<>();
-
-        // countsMap을 내림차순 정렬해 입력 순서를 유지하여 저장
-        countsMap.entrySet()
-            .stream()
-            .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-            .forEachOrdered(x -> reverseSortedMap.put(x.getKey(), x.getValue()));
-
-        // 첫번째 항목 추출
-        Map.Entry<Integer, Integer> entry = reverseSortedMap.entrySet()
-            .iterator()
-            .next();
-
-        // 과반수를 넘을 경우 정답으로 반환
-        return entry.getValue() > nums.length / 2 ? entry.getKey() : -1;
+        
+        // 중앙 위치 인덱스 계산
+        final int mid = left + (right - left) / 2;
+        
+        // 분할 한 배열에서 가장 빈도수가 높은 엘리먼트 추출
+        final int a = majorityElement(left, mid, nums);
+        final int b = majorityElement(mid + 1, right, nums);
+        
+        // 과반이 넘는 엘리먼트 반환
+        int countA = 0;
+        for(int i = left; i <= right; i++) {
+            if(nums[i] == a) {
+                countA++;
+            }
+        }
+        
+        // 과반이 넘는 엘리먼트를 반환
+        return countA > (right - left + 1) / 2 ? a : b;
+    }
+    public int majorityElement(int[] nums) {
+        return majorityElement(0, nums.length - 1, nums);
     }
 }
