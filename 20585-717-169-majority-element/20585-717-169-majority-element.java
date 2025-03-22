c1268bb51a21e3484ccf5bf15c8e9026a6c8a693
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-03-22 19:16:49 +0900

    Time: 4 ms, Memory: 53.4 MB - LeetHub

--- 20585-717-169-majority-element/20585-717-169-majority-element.java
+++ 20585-717-169-majority-element/20585-717-169-majority-element.java
@@ -1,29 +1,6 @@
 class Solution {
-    public int majorityElement(int left, int right, int[] nums) {
-        // 최소 분할 단위 리턴
-        if(left == right) {
-            return nums[left];
-        }
-        
-        // 중앙 위치 인덱스 계산
-        final int mid = left + (right - left) / 2;
-        
-        // 분할 한 배열에서 가장 빈도수가 높은 엘리먼트 추출
-        final int a = majorityElement(left, mid, nums);
-        final int b = majorityElement(mid + 1, right, nums);
-        
-        // 과반이 넘는 엘리먼트 반환
-        int countA = 0;
-        for(int i = left; i <= right; i++) {
-            if(nums[i] == a) {
-                countA++;
-            }
-        }
-        
-        // 과반이 넘는 엘리먼트를 반환
-        return countA > (right - left + 1) / 2 ? a : b;
-    }
     public int majorityElement(int[] nums) {
-        return majorityElement(0, nums.length - 1, nums);
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
     }
 }
