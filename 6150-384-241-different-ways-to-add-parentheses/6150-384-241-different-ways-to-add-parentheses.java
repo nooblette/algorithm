ddbca718dc7c95954e5cff8c9e7f87680e01ecb0
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-03-26 06:23:23 +0900

    Time: 2 ms, Memory: 42 MB - LeetHub

--- 6150-384-241-different-ways-to-add-parentheses/6150-384-241-different-ways-to-add-parentheses.java
+++ 6150-384-241-different-ways-to-add-parentheses/6150-384-241-different-ways-to-add-parentheses.java
@@ -1,6 +1,14 @@
 class Solution {
+    // 표현식의 계산 결과를 저장
+    HashMap<String, List<Integer>> memo = new HashMap<>();
+    
     // 주어진 문자열을 연산자를 기준으로 좌, 우 분할
     public List<Integer> diffWaysToCompute(String expression) {
+        // 이미 계산해둔 동일한 표현식이라면 기존 결과 재활용
+        if(memo.containsKey(expression)) {
+			return memo.get(expression);
+	    }
+        
         // 좌, 우로 분할한 연산 결과에 대해 연산한 결과 목록을 저장
         List<Integer> result = new ArrayList<>();
 
@@ -29,6 +37,9 @@ class Solution {
         if(result.isEmpty()) {
             return List.of(Integer.parseInt(expression));
         }
+        
+        // 계산 결과는 추후 재활용을 위해 저장
+        memo.put(expression, result);
 
         return result;
     }
