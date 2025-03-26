79c7cbc5d160b8a2cdeb82156af2e3c23c8559a9
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-03-26 21:22:21 +0900

    Time: 0 ms, Memory: 40.3 MB - LeetHub

--- 8603-382-509-fibonacci-number/8603-382-509-fibonacci-number.java
+++ 8603-382-509-fibonacci-number/8603-382-509-fibonacci-number.java
@@ -1,9 +1,19 @@
 class Solution {
+    // 문제 조건 : 0 <= n <= 30
+    int[] dp = new int[31];
+    
     public int fib(int n) {
         if(n <= 1) {
             return n;
         }
         
-        return fib(n-1) + fib(n-2);
+        // 미리 계산한 결과가 있다면 재활용
+        if(dp[n] != 0) {
+            return dp[n];
+        }
+        
+        // 계산 결과를 dp 배열에 추가 (기억해둔다.)
+        dp[n] = fib(n-1) + fib(n-2); 
+        return dp[n];
     }
 }
