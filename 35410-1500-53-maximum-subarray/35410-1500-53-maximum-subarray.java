0510d3980b4a5b7050112c791490d673378e3965
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-03-31 19:44:06 +0900

    Time: 5 ms, Memory: 57 MB - LeetHub

--- 35410-1500-53-maximum-subarray/35410-1500-53-maximum-subarray.java
+++ 35410-1500-53-maximum-subarray/35410-1500-53-maximum-subarray.java
@@ -1,15 +1,13 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        // 첫 번째 값을 초기값으로 리스트 선언
-        List<Integer> sums = new ArrayList<>(List.of(nums[0]));
         
         // 이전 값을 메모이제이션
         for(int i = 1; i < nums.length; i++) {
             // 이전까지의 합이 0보다 크면 누적 합산
-            int prev = sums.get(i - 1);
-            sums.add(nums[i] + (prev > 0 ? prev : 0));
+            int prev = nums[i - 1];
+            nums[i] = nums[i] + (prev > 0 ? prev : 0); 
         }
         
-        return Collections.max(sums);
+        return Arrays.stream(nums).max().getAsInt();
     }
 }
