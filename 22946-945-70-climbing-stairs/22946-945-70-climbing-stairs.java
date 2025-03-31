dff679edb8d1aac527192a8cee4ec1b2d51d7d40
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-03-31 20:31:30 +0900

    Time: 0 ms, Memory: 40.5 MB - LeetHub

--- 22946-945-70-climbing-stairs/22946-945-70-climbing-stairs.java
+++ 22946-945-70-climbing-stairs/22946-945-70-climbing-stairs.java
@@ -2,17 +2,17 @@ class Solution {
     int[] dp = new int[46];
     
     public int climbStairs(int n) {
-        // 2이하는 지정한 값 반환 (1인 경우 한가지, 2인 경우 2가지만 존재한다.)
         if(n <= 2) {
             return n;
         }
         
-        // 미리 계산한 결과가 있다면 해당 값 반환
-        if(dp[n] != 0) {
-            return dp[n];
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i = 3; i <= n; i++) {
+            dp[i] = dp[i-2] + dp[i-1];
         }
         
-        dp[n] = climbStairs(n - 2) + climbStairs(n - 1);
         return dp[n];
     }
 }
