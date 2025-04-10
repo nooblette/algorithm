63dc10dffde1a8497afc0b6fc6ad53356f9d0339
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-04-10 23:34:55 +0900

    [level 3] Title: 양과 늑대, Time: 13.18 ms, Memory: 93.8 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,52 +1,66 @@
 import java.util.*;
 
 class Solution {
-    int maxSheep = 0;
+    // from -> to 인접 리스트
+    Map<Integer, List<Integer>> graph = new HashMap<>();
+    int answer = 1;
+
+    static class Node {
+        int index;
+        int sheep;
+        int wolves;
+        List<Integer> nodes;
+
+        Node(int index, int sheep, int wolves, List<Integer> nodes) {
+            this.index = index;
+            this.sheep = sheep;
+            this.wolves = wolves;
+            this.nodes = nodes;
+        }
+    }
     
     public int solution(int[] info, int[][] edges) {
-        // 부모 - 자식 관계 초기화
-        List<List<Integer>> graph = new ArrayList<>();
-        for(int i = 0; i < info.length; i++) {
-            graph.add(new ArrayList<>());
-        }
+        // from -> to 그래프 구성
         for(int[] edge : edges) {
+            graph.putIfAbsent(edge[0], new ArrayList<>());
             graph.get(edge[0]).add(edge[1]);
         }
         
-        // 0번 노드부터 dfs 탐색 
-        dfs(0, 0, 0, List.of(0), info, graph);
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(new Node(0, 1, 0, graph.get(0)));
         
-        return maxSheep;
-    }
-    
-    private void dfs(int current, int sheep, int wolf, List<Integer> nextNodes, int[] info, List<List<Integer>> graph) {
-        // 현재 노드 방문 - 양과 늑대의 수 갱신
-        if(info[current] == 0) {
-            sheep++;
-        } else {
-            wolf++;
+        while(!queue.isEmpty()) {
+            Node node = queue.poll();
+            
+            // 방문 가능한 노드 목록 탐색
+            for(int nextNode : node.nodes) {
+                // Node 클래스의 양의 수(sheep)와 늑대의 수(wolves)를 상태 갱신
+                int nextSheep = (info[nextNode] == 0) ? node.sheep + 1 : node.sheep;
+                int nextWolves = (info[nextNode] == 1) ? node.wolves + 1 : node.wolves;
+
+                // 방문할 수 있다면
+                if(nextSheep > nextWolves) {
+                    // 최대값 갱신
+                    answer = Math.max(answer, nextSheep);
+
+                    // 기존 node.nodes를 유지(복사하여 새로운 객체 참조)하고 방문한 노드의 자식 노드를 추가
+                    // 값에 의한 호출이 되도록 완전히 새로운 리스트를 복사하여 생성한다.
+                    List<Integer> candidateNodes = new ArrayList<>(node.nodes);
+
+                    // 현재 방문한 노드는 제외
+                    candidateNodes.remove(Integer.valueOf(nextNode));
+
+                    // 방문한 노드의 자식 노드를 추가
+                    if(graph.get(nextNode) != null) {
+                        candidateNodes.addAll(graph.get(nextNode));
+                    }
+
+                    // 큐에 삽입
+                    queue.offer(new Node(nextNode, nextSheep, nextWolves, candidateNodes));
+                }
+            }
         }
         
-        // 더 이상 진행할 수 없다면 현재 경로상 current 노드는 포기
-        // 다른 노드에서 양을 더 얻고 나면 방문 가능할 수 있음
-        if(sheep <= wolf) {
-            return;
-        }
-        
-        // 최대값 갱신
-        maxSheep = Math.max(maxSheep, sheep);
-        
-        // 다음에 방문할 후보 리스트 갱신 -리스트를 수정하면 탐색 경로에 영향을 끼치므로(주소를 참조) 새로운 변수에 할당한다.
-        // 기존에 방문 후보였던 노드(복사) + 현재 노드의 자식 노드를 추가(확장)
-        List<Integer> newNextNodes = new ArrayList<>(nextNodes);
-        
-        // 현재 방문한 경로는 제외하고 자식 노드를 추가
-        newNextNodes.remove(Integer.valueOf(current));
-        newNextNodes.addAll(graph.get(current));
-        
-        // 자식 노드를 확장한 방문 후보 노드에 대해 탐색 반복
-        for(int next : newNextNodes) {
-            dfs(next, sheep, wolf, newNextNodes, info, graph);    
-        }
+        return answer;
     }
 }
