a4852b4828251f4bba2e002656e6208d7833b735
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-04-29 21:21:14 +0900

    No Parameter Lambda Expression

---
+++ geeksforgeeks/no-parameter-lambda-expression/GFG.java
@@ -0,0 +1,61 @@
+// https://www.geeksforgeeks.org/problems/no-parameter-lambda-expression/1
+//{ Driver Code Starts
+// Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+
+        // taking input using Scanner class
+        Scanner sc = new Scanner(System.in);
+
+        // taking testcases count
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            // calling helperFunction of class Hello
+            Hello h = helperFunction();
+
+            // calling Interface method
+            h.sayHello();
+
+            System.out.println("~");
+        }
+    }
+
+    
+// } Driver Code Ends
+
+// User function Template for Java
+/*
+This is what the interface looks like
+interface Hello {
+    public void sayHello();
+}
+*/
+public static class MyHello implements Hello {
+    @Override
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+}
+
+public static Hello helperFunction() {
+    // Your code here
+    return new MyHello();
+    // Implement sayHello using lambda expression and return the object.
+    // Write this in the lambda expression: System.out.println("Hello")
+}
+
+
+//{ Driver Code Starts.
+}
+
+interface Hello {
+    public void sayHello();
+}
+// } Driver Code Ends
