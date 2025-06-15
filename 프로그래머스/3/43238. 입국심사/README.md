f8a4aaf077e5c6d900dc51a70fa577d7192ea9bf
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-06-15 21:29:57 +0900

    [level 3] Title: 입국심사, Time: 65.86 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=java) 
 
 ### 성능 요약
 
-메모리: 67 MB, 시간: 57.41 ms
+메모리: 84.9 MB, 시간: 65.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 01일 16:49:48
+2025년 06월 15일 21:29:54
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,35 +2,36 @@ import java.util.Arrays;
 
 class Solution {
     public long solution(int n, int[] times) {
+        // 주어진 시간 동안 심사할 수 있는 인원을 기준으로 이분 탐색
         long left = 1;
         
-        // 맨 오른쪽은 (입력 값 중 가장 큰 값 * n)을 최댓값으로 지정
+        // 가능한 최대 시간(가장 오래걸리는 심사관의 소요 시간 * 모든 인원)으로 초기화
         long right = (long) Arrays.stream(times).max().getAsInt() * n;
         
-        // 초기 중앙 값은 최대값으로 설정
+        // 심사할 수 있는 인원의 초기 중앙 값은 최대값으로 설정
         long mid = right;
         
         long answer = 0;
         while(left <= right) {
-            // 각 심사관이 주어진 mid 시간 동안 심사할 수 있는 최대 인원 계산
+            // 각 심사관이 주어진 시간(mid)동안 심사할 수 있는 최대 인원 계산
             long maxImmigrantsInMid = 0;
             for(int time : times) {
                 maxImmigrantsInMid += (mid / time);
             }
             
-            // n을 기준으로 이분탐색하여 n명을 심사할 수 있는 최소시간을 구한다.
             if(n <= maxImmigrantsInMid) {
-                // 최대 심사가능한 인원이 n보다 크거나 같은 경우, 최소 시간을 구하기 위해 범위를 줄여서 반복한다.
+                // mid시간 동안 심사가능한 인원이 n보다 큰 경우 -> 최소 시간을 찾아야한다.
+                // 따라서 mid를 기준으로 왼쪽에 대해 이분 탐색 반복
                 answer = mid;
                 right = mid - 1;
             } else {
-                // 최대 심사 가능한 인원이 n보다 작은 경우 시간 범위를 늘린다.
+                // mid시간 동안 심사가능한 인원이 n보다 작은 경우 -> 모든 인원을 심사할 수 없으므로 시간 범위를 늘린다.
                 left = mid + 1;
             }
             
-            // 중앙 값 갱신
             mid = left + (right - left) / 2;
         }
+        
         return answer;
     }
 }
