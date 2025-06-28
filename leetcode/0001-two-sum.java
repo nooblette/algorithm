e74f34faeebce239223a169a59740f84a65ec92e
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-06-28 14:00:35 +0900

    Update 0001-two-sum.java

--- leetcode/0001-two-sum.java
+++ leetcode/0001-two-sum.java
@@ -1,17 +1,19 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.length; i++) {
-            for(int j = i; j < nums.length; j++) {
-                // 동일한 인덱스는 건너뛴다.
-                if(i == j){
-                    continue;
-                }
+        // 배열을 탐색하며 만난 요소와 등장한 인덱스를 기록
+        Map<Integer, Integer> seen = new HashMap<>();
 
-                // 정답을 찾은 경우
-                if(nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+        for(int i = 0; i < nums.length; i++) {
+            // target을 만들기 위해 필요한 값
+            int need = target - nums[i];
+
+            // 이미 등장한적이 있다면 정답 반환
+            if(seen.containsKey(need)) {
+                return new int[]{seen.get(need), i};
             }
+
+            // 없다면 Map에 기록 후 다음 원소 탐색
+            seen.put(nums[i], i);
         }
 
         // 예외 처리
