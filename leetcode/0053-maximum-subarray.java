5c812370c98c175e82dec036ad27b84e6f42e084
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-07-05 11:13:04 +0900

    Update 0053-maximum-subarray.java

--- leetcode/0053-maximum-subarray.java
+++ leetcode/0053-maximum-subarray.java
@@ -1,18 +1,14 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int sumOfSubArray = Integer.MIN_VALUE;
-        int answer = Integer.MIN_VALUE;
+        int sumOfSubArray = 0;
+
+        // 음수 오버플로우를 방지하기 위해 Integer.MIN_VALUE가 아닌 첫번째 값으로 초기화한다.
+        int answer = nums[0];
 
         // 누적합 계산
         for(int num : nums) {
-            // 현재까지의 누적합이 음수인 경우 새로 시작한다.
-            if(sumOfSubArray < 0) {
-                sumOfSubArray = num;
-            } else {
-                // 현재까지의 누적합이 양수인 경우 다음 계산에 사용한다.
-                sumOfSubArray += num;
-            }
-            
+            // 현재까지 구한 값에 누적하는 경우와 새로 시작하는 경우 중 더 큰 값을 취한다.
+            sumOfSubArray = Math.max(sumOfSubArray + num, num);
             answer = Math.max(answer, sumOfSubArray);
         }
 
