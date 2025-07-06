7656da9eba3d8ff5f12a06b803f92897b26938d0
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-07-06 23:43:20 +0900

    Update 0033-search-in-rotated-sorted-array.java

--- leetcode/0033-search-in-rotated-sorted-array.java
+++ leetcode/0033-search-in-rotated-sorted-array.java
@@ -1,23 +1,4 @@
 class Solution {
-    private int binarySearch(int left, int right, int[] nums, int target) {
-        while(left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if(nums[mid] == target) {
-                return mid;
-            } else if(nums[mid] < target) {
-                // target이 오른쪽 범위에 속하는 경우
-                left = mid + 1;
-            } else {
-                // target이 왼쪽 범위에 속하는 경우
-                right = mid - 1;
-            }
-        }
-
-        // target이 주어진 배열에 속하지 않는 경우
-        return -1;
-    }
-
     public int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
@@ -25,13 +6,18 @@ class Solution {
         while(left <= right) {
             int mid = left + (right - left) / 2;
 
+            // 정답을 찾은 경우 탐색 종료
+            if(nums[mid] == target) {
+                return mid;
+            }
+
             // 이분 탐색이 가능한 정렬된 구간을 찾는다.
             // 왼쪽 구간이 정렬된 경우 (left와 mid가 동일할 수 있으므로 <=으로 비교한다.)
             if(nums[left] <= nums[mid]) {
                 // 정렬된 구간에 target이 포함되는 경우
                 if(nums[left] <= target && target <= nums[mid]) {
                     // 이 구간에 대해 이분 탐색한다.
-                    return binarySearch(left, mid, nums, target);
+                    right = mid;
                 } else {
                     // 정렬된 구간에 target이 포함되지 않은 경우
                     // 오른쪽 범위에 대해 다시 정렬된 구간을 찾는 과정을 반복한다.
@@ -42,7 +28,7 @@ class Solution {
                 // 정렬된 구간에 target이 포함되는 경우
                 if(nums[mid] <= target && target <= nums[right]) {
                     // 이 구간에 대해 이분 탐색한다.
-                    return binarySearch(mid, right, nums, target);
+                    left = mid;
                 } else {
                     // 정렬된 구간에 target이 포함되지 않은 경우
                     // 왼쪽 범위에 대해 다시 정렬된 구간을 찾는 과정을 반복한다.
