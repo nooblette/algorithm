cf38ed627ba5673cd24d880e288114c4b6ea3d3c
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-07-07 00:13:55 +0900

    Update 0015-3Sum.java

--- leetcode/0015-3Sum.java
+++ leetcode/0015-3Sum.java
@@ -3,9 +3,13 @@ class Solution {
         // 정렬
         Arrays.sort(nums);
 
-        // 중복 제거를 위해 Set<>으로 선언한다.
-        Set<List<Integer>> answer = new HashSet<>();
+        List<List<Integer>> answer = new ArrayList<>();
         for(int i = 0; i < nums.length; i++) {
+            // 중복된 i 값 건너뛰기
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+
             int left = i + 1;
             int right = nums.length - 1;
 
@@ -13,16 +17,26 @@ class Solution {
             while(left < right) {
                 if(nums[i] + nums[left] + nums[right] > 0) {
                     // 총 합이 감소하는 방향으로 이동한다.
-                    right -= 1;
+                    right--;
                 } else if(nums[i] + nums[left] + nums[right] < 0) {
                     // 총 합이 증가하는 방향으로 이동한다.
-                    left += 1;
+                    left++;
                 } else {
                     // 정답을 찾은 경우
                     answer.add(List.of(nums[i], nums[left], nums[right]));
 
-                    // 위 과정 반복
-                    left += 1;
+                    // 동일한 값인 동안 left와 right을 이동하여 중복을 방지한다.
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--; 
+                    }
+
+                    // left와 right를 함께 이동하여 탐색 범위를 줄인다.
+                    left++;
+                    right--;
                 }
             }
         }
