2c538e28e44e6d626cfba61af3179e1cbbf8cd22
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-07-20 10:09:27 +0900

    Update 0268-missing-number.java

--- leetcode/0268-missing-number.java
+++ leetcode/0268-missing-number.java
@@ -1,7 +1,20 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        // 0부터 n까지의 합 - 주어진 배열의 원소들의 합
         int n = nums.length;
-        return (n * (n + 1) / 2) - Arrays.stream(nums).sum();
+
+        int result = 0;
+
+        // 0부터 n까지 모든 수에 대해 XOR 연산을 취한다.
+        for(int i = 0; i <= n; i++) {
+            result ^= i;
+        }
+
+        // 각 원소에 대해 XOR 연산을 취한다.
+        for(int num : nums) {
+            result ^= num;
+        }
+
+        // 누락되지 않은 수는 2번 등작하여 XOR 연산에 의해 상쇄되고, 누락된 수만 1번 등장하여 남게된다.
+        return result;
     }
 }
