a14b5be8789a5d196ddf7f8c08065711207033fd
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-07-20 09:38:50 +0900

    Update 0338-counting-bits.java

--- leetcode/0338-counting-bits.java
+++ leetcode/0338-counting-bits.java
@@ -3,7 +3,8 @@ class Solution {
         int[] dp = new int[n+1];
 
         for(int i = 1; i <= n; i++) {
-            dp[i] = dp[i >> 1] + (i & 1);
+            // dp[i & (i-1)] : i에서 뒤에서 가장 먼저 나온 1비트를 삭제한 결과의 1의 개수
+            dp[i] = dp[i & (i-1)] + 1;
         }
 
         return dp;
