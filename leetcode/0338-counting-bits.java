1a4337b16a767ca5fc611a4b001c80fddbb1952d
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-07-20 09:33:08 +0900

    Update 0338-counting-bits.java

--- leetcode/0338-counting-bits.java
+++ leetcode/0338-counting-bits.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] ans = new int[n+1];
+        int[] dp = new int[n+1];
 
-        for(int i = 0; i <= n; i++) {
-            ans[i] = Integer.bitCount(i);
+        for(int i = 1; i <= n; i++) {
+            dp[i] = dp[i >> 1] + (i & 1);
         }
 
-        return ans;
+        return dp;
     }
 }
