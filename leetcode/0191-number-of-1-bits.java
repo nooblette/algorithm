d3c68638d94e8843e3e765945f92cba86af3e01f
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-07-20 09:09:44 +0900

    Update 0191-number-of-1-bits.java

--- leetcode/0191-number-of-1-bits.java
+++ leetcode/0191-number-of-1-bits.java
@@ -1,12 +1,5 @@
 class Solution {
     public int hammingWeight(int n) {
-        int count = 0;
-        
-        while(n > 0) {
-            n &= n - 1;
-            count++;
-        }
-
-        return count;
+        return Integer.bitCount(n);
     }
 }
