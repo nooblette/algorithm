72bf254630bd3aa3059b44118c9a8d56a064308f
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-07-28 11:27:01 +0900

    Update 0190-reverse-bits.java

--- leetcode/0190-reverse-bits.java
+++ leetcode/0190-reverse-bits.java
@@ -1,13 +1,18 @@
 class Solution {
     public int reverseBits(int n) {
-        // 주어진 정수를 비트 문자열로 변환
-        // 앞자리가 0이 오는 경우를 보존하기 위해 String.format()으로 32자리 패딩한다.
-        String binaryN = String.format("%32s", Integer.toBinaryString(n)).replace(' ', '0');
+        int result = 0;
 
-        // 비트 문자열을 뒤집는다.
-        StringBuilder sb = new StringBuilder(binaryN);
-        String reversed = sb.reverse().toString();
-        
-        return Integer.parseInt(reversed, 2);
+        for(int i = 0; i < 32; i++) {
+            // result 좌측 shift
+            result <<= 1;
+
+            // 주어진 정수 n의 가장 우측 비트 추출하여 result에 할당
+            result |= (n & 1);
+
+            // 정수 n 우측 shift
+            n >>= 1;
+        }
+
+        return result;
     }
 }
