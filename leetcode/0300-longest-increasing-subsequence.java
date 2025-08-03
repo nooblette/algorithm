0c6d6589f6cb574c864025b0e07c463a9f752fa8
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-08-03 11:48:04 +0900

    Update 0300-longest-increasing-subsequence.java

--- leetcode/0300-longest-increasing-subsequence.java
+++ leetcode/0300-longest-increasing-subsequence.java
@@ -1,27 +1,48 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int answer = 1;
-        
-        // 인덱스 별로 최장 부분 배열 길이 저장
-        int[] dp = new int[n];
 
-        // 초기화 - 모든 위치에서 최소 길이는 1
-        for(int i = 0; i < n; i++) {
-            dp[i] = 1;
-        }
-        
-        for(int i = 1; i < n; i++) {
-            for(int j = 0; j < i; j++) {
-                // 이전 원소 중 현재 원소보다 작은 값부터 1 증가
-                if(nums[i] > nums[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
+        // 인덱스별로 가능한 가장 작은 원소들로 이루어진 부분 수열 배열 선언
+        // BinarySearch 대상 배열이 된다.
+        List<Integer> subsequence = new ArrayList<>();
+
+        for(int num : nums) {
+            // 현재 원소 num이 부분 수열 배열에 들어갈 위치 탐색
+            int pos = binarySearch(subsequence, num);
+
+            // 부분 수열 배열의 모든 원소가 현재 원소보다 작다면 배열에 새로 추가
+            if(pos == subsequence.size()) {
+                subsequence.add(num);
+            } else {
+                // 부분 수열 배열 중 현재 원소보다 큰 값이 있다면, 현재 원소보다 크거나 같은 첫번째 값을 현재 원소로 교체  
+                subsequence.set(pos, num);
             }
-
-            answer = Math.max(answer, dp[i]);
         }
 
-        return answer;
+        // 부분 수열 배열의 최종 길이를 반환
+        return subsequence.size();
+    }
+
+    private int binarySearch(List<Integer> subsequence, int target) {
+        int left = 0;
+        int right = subsequence.size();
+
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+
+            // target이 탐색하는 배열의 중앙보다 우측에 있다면
+            if(subsequence.get(mid) < target) {
+                // 시작 지점을 변경
+                left = mid + 1;
+            } else {
+                // target이 탐색하는 배열의 중앙보다 좌측에 있다면
+                // 종료 지점을 변경
+                right = mid;
+            }
+        }
+
+        // target이 들어갈 위치를 반환
+        return left;
+
     }
 }
