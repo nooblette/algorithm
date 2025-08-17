2dc3a6aa2872c4a113dc4f90b0a9c515956f88c9
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-08-17 09:54:42 +0900

    Update 0200-number-of-islands.java

--- leetcode/0200-number-of-islands.java
+++ leetcode/0200-number-of-islands.java
@@ -5,37 +5,34 @@ class Solution {
 
         int result = 0;
 
-        // 방문 기록 저장
-        boolean[][] visited = new boolean[r][c];
         for(int i = 0; i < r; i++) {
             for(int j = 0; j < c; j++) {
-                if(grid[i][j] == '0' || visited[i][j] == true) {
+                if(grid[i][j] == '0') {
                     continue;
                 }
                 // 탐색 시작 지점만큼 증가
                 result++;
-                dfs(i, j, grid, visited, r, c);
+                dfs(i, j, grid, r, c);
             }
         }
 
         return result;
     }
 
-    private void dfs(int curR, int curC, char[][] grid, boolean[][] visited, int r, int c) {
-        if(curR < 0 || curR >= r || curC < 0 || curC >= c || grid[curR][curC] == '0' || visited[curR][curC] == true) {
+    private void dfs(int curR, int curC, char[][] grid, int r, int c) {
+        if(curR < 0 || curR >= r || curC < 0 || curC >= c || grid[curR][curC] == '0') {
             return;
         }
 
-        // 방문 기록 저장
-        visited[curR][curC] = true;
+        // 방문한 섬은 물로 변경한다.
+        grid[curR][curC] = '0';
 
         // 상하좌우
         int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         for(int[] direction : directions) {
             int newR = curR + direction[0];
             int newC = curC + direction[1];
-            dfs(newR, newC, grid, visited, r, c);
+            dfs(newR, newC, grid, r, c);
         }
-
     }
 }
