01bacc56ab67e2266adec02eef5967584230b9a2
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-08-24 14:23:34 +0900

    Create 0021-merge-two-sorted-lists.java

---
+++ leetcode/0021-merge-two-sorted-lists.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode merged = new ListNode();
+        ListNode head = merged;
+
+        // 연결 리스트 탐색
+        while(list1 != null && list2 != null) {
+            // 오름차순을 유지하는 동안 연결
+            while((list1 != null && list2 != null) && (list1.val <= list2.val)) {
+                merged.next = list1;
+
+                list1 = list1.next;
+                merged = merged.next;
+            }
+
+            while((list1 != null && list2 != null) && (list2.val <= list1.val)) {
+                merged.next = list2;
+
+                list2 = list2.next;
+                merged = merged.next;
+            }
+        }
+
+        // 남은 노드 연결
+        while(list1 == null && list2 != null) {
+            merged.next = list2;
+            list2 = list2.next;
+            merged = merged.next;
+        }
+
+        while(list1 != null && list2 == null) {
+            merged.next = list1;
+            list1 = list1.next;
+            merged = merged.next;
+        }
+
+        return head.next;
+    }
+}
