4dd4830cc6b2a447c73574e4fcadb79c2723ecd9
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2026-01-06 21:43:24 +0900

    Update 0020-valid-parentheses.java

--- leetcode/0020-valid-parentheses.java
+++ leetcode/0020-valid-parentheses.java
@@ -2,22 +2,16 @@ class Solution {
     public boolean isValid(String s) {
         // 유효성 검증을 위한 스택
         Deque<Character> stack = new ArrayDeque<>();
-
-        List<Character> openParentheses = List.of('(', '{', '[');
-        for(int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-
+        
+        Map<Character, Character> parentheses = Map.of('(', ')', '{', '}', '[', ']');
+        for(char c : s.toCharArray()) {
             // 여는 괄호라면 스택에 추가
-            if(openParentheses.contains(c)) {
+            if(c == '(' || c == '{' || c == '[') {
                 stack.add(c);
             } else {
-                // 스택이 비어있지 않고 닫는 괄호이고 쌍이 맞는다면 제거
-                if(!stack.isEmpty() && (
-                    (c == ')' && stack.peekLast() == '(') || 
-                    (c == '}' && stack.peekLast() == '{') || 
-                    (c == ']' && stack.peekLast() == '['))
-                ) {
-                        stack.removeLast();
+                // 스택이 비어있지 않으면서 가장 위 문자와 쌍이 맞는다면 제거
+                if(!stack.isEmpty() && c == parentheses.get(stack.peekLast())) {
+                    stack.removeLast();
                 } else {
                     // 스택이 도중에 비거나 유효하지 않은 쌍 존재
                     return false;
@@ -26,6 +20,6 @@ class Solution {
         }
 
         // 스택이 비어있다면 모든 쌍이 유효함
-        return stack.isEmpty() ? true : false;
+        return stack.isEmpty();
     }
 }
