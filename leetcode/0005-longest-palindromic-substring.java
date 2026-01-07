0c28a4fe00ba38647ebf70687663c31c43075bd0
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2026-01-07 22:12:13 +0900

    Update 0005-longest-palindromic-substring.java

--- leetcode/0005-longest-palindromic-substring.java
+++ leetcode/0005-longest-palindromic-substring.java
@@ -1,47 +1,31 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int result = 0;
-        int start = 0;
-        int end = 0;
+        int left = 0;
+        int right = 0;
 
         // 중심 확장(Expand Around Center) 알고리즘으로 팰린드롬 여부 판별
         for(int i = 0; i < s.length(); i++) {
-            // 팰린드롬인 동안 좌우를 확장
-            int left = i;
+            int odd = expandAroundCenter(s, i, i);
+            int even = expandAroundCenter(s, i, i + 1);
 
-            // 홀수 길이로 탐색하는 경우
-            int right = i;
-            while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-                left--;
-                right++;
-            }
-
-            // 새로운 최장 길이 팰린드롬 부분 문자열을 찾은 경우 갱신
-            if(result < right - left + 1) {
-                result = right - left + 1;
-
-                // 마지막 반복에서 증감을 복구
-                start = left + 1;
-                end = right - 1;
-            }
-
-            // 짝수 길이로 탐색하는 경우
-            left = i;
-            right = i + 1;
-            while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-                left--;
-                right++;
-            }
-
-            if(result < right - left + 1) {
-                result = right - left + 1;
-
-                // 마지막 반복에서 증감을 복구
-                start = left + 1;
-                end = right - 1;
+            int maxLen = Math.max(odd, even);
+            if(maxLen > right - left + 1) {
+                left = i - (maxLen - 1) / 2;
+                right = i + maxLen / 2;
             }
         }
 
-        return s.substring(start, end + 1);
+        return s.substring(left, right + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        // 두 포인터를 좌우로 확장하며 팰린드롬 여부 판별
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+
+        // 마지막 loop에서 증감분 2를 최종 길이에서 제외한다.
+        return right - left - 1;
     }
 }
