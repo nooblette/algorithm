df4117bacf2a9172392bff00289ab8844d7a4a4f
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2026-01-18 21:49:42 +0900

    Create 0100-same-tree.java

---
+++ leetcode/0100-same-tree.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // Null 처리
+        if(p == null && q == null) {
+            return true;
+        } else if(p == null || q == null) {
+            return false;
+        }
+
+        // 현재 노드의 값이 동일하다면 자식 노드의 값도 동일한지 비교
+        if(p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+
+        return false;
+    }
+}
