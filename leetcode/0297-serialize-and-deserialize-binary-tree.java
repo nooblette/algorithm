4c0f2f7fadb1898f29995e2c3cc11d28fab25575
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2026-01-25 11:08:07 +0900

    Update 0297-serialize-and-deserialize-binary-tree.java

--- leetcode/0297-serialize-and-deserialize-binary-tree.java
+++ leetcode/0297-serialize-and-deserialize-binary-tree.java
@@ -11,68 +11,46 @@ public class Codec {
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        StringBuffer sb = new StringBuffer("[");
-
-        // null을 추가하기 위해 LinkedList로 구현
-        Deque<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        while(!queue.isEmpty()) {
-            TreeNode cur = queue.poll();
-
-            if(cur == null) {
-                sb.append("null,");
-            } else {
-                sb.append(cur.val + ",");
-                queue.offer(cur.left);
-                queue.offer(cur.right);
-            }
-        }
+        // DFS (Preorder)
+        StringBuilder sb = new StringBuilder();
+        dfs(root, sb);
         
-        // sb의 마지막 문자가 ','일 때
-        if (sb.charAt(sb.length() - 1) == ',') {
-            sb.setCharAt(sb.length() - 1, ']'); // 마지막 ','를 ']'로 변경
-        } else {
-            sb.append("]");
-        }
+        // Input: [1,2,3,null,null,4,5,6,7]
+        // Output(Level order): 1,2,null,null,3,4,6,null,7,null,5,null,null,
         return sb.toString();
+    }
+
+    private void dfs(TreeNode root, StringBuilder sb) {
+        if(root == null) {
+            sb.append("null,");
+            return;
+        }
+
+        sb.append(root.val).append(",");
+        dfs(root.left, sb);
+        dfs(root.right, sb);
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        // 예외 처리
-        if("[]".equals(data)) {
+        // DFS (Preorder)
+        Deque<String> queue = new ArrayDeque<>(Arrays.asList(data.split(",")));
+
+        // Input: 1,2,null,null,3,4,6,null,7,null,5,null,null,
+        // Output(Pre order): [1, 2, null, null, 3, 4, 6, null, 7, null, 5, null, null]
+        return dfs(queue);
+    }
+
+    private TreeNode dfs(Deque<String> queue) {
+        String val = queue.poll();
+        if("null".equals(val)) {
             return null;
         }
 
-        // 문자열을 값 배열로 변환
-        String[] vals = data.substring(1, data.length() - 1).split(",");
-        if("null".equals(vals[0])) {
-            return null;
-        }
-
-        Deque<TreeNode> queue = new ArrayDeque();
-
-        TreeNode root = new TreeNode(Integer.parseInt(vals[0]));
-        queue.offer(root);
-        for(int i = 1; !queue.isEmpty() && i < vals.length; i += 2) {
-            TreeNode cur = queue.poll();
-
-            // 왼쪽 자식 연결
-            if(!"null".equals(vals[i])) {
-                TreeNode left = new TreeNode(Integer.parseInt(vals[i]));
-                cur.left = left;
-                queue.offer(left);
-            }
-
-            // 오른쪽 자식 연결
-            if(!"null".equals(vals[i + 1])) {
-                TreeNode right = new TreeNode(Integer.parseInt(vals[i + 1]));
-                cur.right = right;
-                queue.offer(right);
-            }
-        }
-
-        return root;
+        TreeNode node = new TreeNode(Integer.parseInt(val));
+        node.left = dfs(queue);
+        node.right = dfs(queue);
+        return node;
     }
 }
 
