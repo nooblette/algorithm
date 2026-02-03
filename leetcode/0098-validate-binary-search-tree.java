86102330a3f506d7bc43a5126fa3788479c38ac8
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2026-02-03 20:32:32 +0900

    Update 0098-validate-binary-search-tree.java

--- leetcode/0098-validate-binary-search-tree.java
+++ leetcode/0098-validate-binary-search-tree.java
@@ -15,20 +15,31 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        // BST를 유지하기 위해서 각 노드가 속할 수 있는 범위 내에 있는지 검증
-        return validRange(root, null, null);
-    }
+        // 트리를 중위 순회(left -> root -> right)하여 검증, 단 재귀가 아닌 스택을 활용
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode cur = root;
+        Integer prev = null;
 
-    private boolean validRange(TreeNode node, Integer min, Integer max) {
-        if(node == null) {
-            return true;
+        while(cur != null || !stack.isEmpty()) {
+            // 가장 왼쪽 서브트리로 이동
+            while(cur != null) {
+                stack.offer(cur);
+                cur = cur.left;
+            }
+
+            // BST를 만족하는지 확인, 중위 순회 중이므로 이전에 현재 val보다 큰 값이 존재해선 안된다.
+            TreeNode node = stack.pollLast();
+            if(prev != null && prev >= node.val) {
+                return false;
+            }
+            prev = node.val;
+
+            // 오른쪽 서브트리 탐색을 위함
+            if(node.right != null) {
+                cur = node.right;
+            }
         }
 
-        // 현재 노드가 BST를 유지하는 유효한 범위내에 있는지 검증
-        if((min != null && node.val <= min) || (max != null && node.val >= max)) {
-            return false;
-        }
-
-        return validRange(node.left, min, node.val) && validRange(node.right, node.val, max);
+        return true;
     }
 }
