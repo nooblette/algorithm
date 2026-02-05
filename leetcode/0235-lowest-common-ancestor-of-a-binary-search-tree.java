e349ff825f167c8f34c69bb4bd710be230173402
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2026-02-05 20:56:49 +0900

    Update 0235-lowest-common-ancestor-of-a-binary-search-tree.java

--- leetcode/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ leetcode/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,24 +10,17 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        // 인덱스 비교 없이 주어진 트리 자체만으로 공통 조상을 판별한다.
-        TreeNode node = root;
+        // 인덱스 비교 없이 주어진 트리 자체만으로 공통 조상을 판별한다, 단 재귀 호출하여 풀이한다.
 
-        while(node != null) {
-            int cur = node.val;
-
-            // 현재 node 자체가 공통 조상인 경우
-            if(cur == p.val || cur == q.val || cur > p.val && cur < q.val || cur > q.val && cur < p.val) {
-                return node;
-            } else if(cur > p.val && cur > q.val) {
-                // p와 q가 현재 node 기준으로 왼쪽 서브트리에 있는 경우
-                node = node.left;
-            } else {
-                // p와 q가 root 기준으로 오른쪽 서브트리 있는 경우
-                node = node.right;
-            }
+        // p와 q가 현재 node 기준으로 왼쪽 서브트리에 있는 경우
+        if(root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if(root.val < p.val && root.val < q.val) {
+            // p와 q가 현재 node 기준으로 오른쪽 서브트리에 있는 경우
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            // 현재 node가 공통 조상인 경우
+            return root;
         }
-
-        return node;
     }
 }
