3f0f88dedaedf8b72d2e9851e673f077884f195d
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2026-02-05 20:51:25 +0900

    Update 0235-lowest-common-ancestor-of-a-binary-search-tree.java

--- leetcode/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ leetcode/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,46 +9,25 @@
  */
 
 class Solution {
-    private List<Integer> inorder = new ArrayList<>();
-    private Map<Integer, Integer> valByIndex = new HashMap<>();
-
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        // 주어진 트리를 중위 순회하여 배열 생성
-        generateInorder(root);
+        // 인덱스 비교 없이 주어진 트리 자체만으로 공통 조상을 판별한다.
+        TreeNode node = root;
 
-        // key: node의 val, value: 해당 val이 등장하는 inorder 리스트에서의 인덱스
-        for(int i = 0; i < inorder.size(); i++) {
-            valByIndex.put(inorder.get(i), i);
+        while(node != null) {
+            int cur = node.val;
+
+            // 현재 node 자체가 공통 조상인 경우
+            if(cur == p.val || cur == q.val || cur > p.val && cur < q.val || cur > q.val && cur < p.val) {
+                return node;
+            } else if(cur > p.val && cur > q.val) {
+                // p와 q가 현재 node 기준으로 왼쪽 서브트리에 있는 경우
+                node = node.left;
+            } else {
+                // p와 q가 root 기준으로 오른쪽 서브트리 있는 경우
+                node = node.right;
+            }
         }
 
-        // BST를 중위 순회하여 오름차순 정렬한 배열로 탐색하므로 left에 작은 값이 들어가야한다.
-        return findAncestor(root, Math.min(p.val, q.val), Math.max(p.val, q.val));
-    }
-
-    private TreeNode findAncestor(TreeNode node, int p, int q) {
-        int cur = node.val;
-
-        // 현재 node 자체가 공통 조상인 경우
-        if(cur == p || cur == q || valByIndex.get(cur) > valByIndex.get(p) && valByIndex.get(cur) < valByIndex.get(q)) {
-            return node;
-        }
-
-        // p와 q가 root 기준으로 왼쪽 서브트리에 있는 경우
-        if(valByIndex.get(cur) > valByIndex.get(p) && valByIndex.get(cur) > valByIndex.get(q)) {
-            return findAncestor(node.left, p, q);
-        }
-
-        // p와 q가 root 기준으로 오른쪽 서브트리 있는 경우
-        return findAncestor(node.right, p, q);
-    }
-
-    private void generateInorder(TreeNode node) {
-        if(node == null) {
-            return;
-        }
-
-        generateInorder(node.left);
-        inorder.add(node.val);
-        generateInorder(node.right);
+        return node;
     }
 }
