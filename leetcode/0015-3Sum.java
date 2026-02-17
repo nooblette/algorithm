fe24294d018edebcaf153be5f7810db4f9fa6374
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2026-02-17 19:21:27 +0900

    Update 0015-3Sum.java

--- leetcode/0015-3Sum.java
+++ leetcode/0015-3Sum.java
@@ -1,46 +1,50 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        // 정렬
+        // 투 포인터 탐색을 위해 주어진 배열을 정렬
         Arrays.sort(nums);
 
-        List<List<Integer>> answer = new ArrayList<>();
-        for(int i = 0; i < nums.length; i++) {
-            // 중복된 i 값 건너뛰기
-            if (i > 0 && nums[i] == nums[i-1]) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        // 고정 인덱스 정의
+        for(int i = 0; i < nums.length - 2; i++) {
+            // 고정 인덱스 중복 처리
+            if(i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
 
+            // 3개 포인터가 가리키는 값의 합이 0이 되는 방향으로 투 포인터 탐색
             int left = i + 1;
             int right = nums.length - 1;
-
-            // left와 right를 이동하며 i + left + right = 0이 되는 left와 right를 찾는다.
+            
             while(left < right) {
-                if(nums[i] + nums[left] + nums[right] > 0) {
-                    // 총 합이 감소하는 방향으로 이동한다.
+                int sum = nums[i] + nums[left] + nums[right];
+
+                // 총 합이 0보다 크다면 총 합이 감소하는 방향으로 탐색
+                if(sum > 0) {
                     right--;
-                } else if(nums[i] + nums[left] + nums[right] < 0) {
-                    // 총 합이 증가하는 방향으로 이동한다.
+                } else if(sum < 0){
+                    // 총 합이 0보다 작다면 총 합이 증가하는 방향으로 탐색
                     left++;
                 } else {
-                    // 정답을 찾은 경우
-                    answer.add(List.of(nums[i], nums[left], nums[right]));
+                    // 총 합이 0이라면 정답 배열에 추가하고 다음 범위 탐색
+                    result.add(List.of(nums[i], nums[left], nums[right]));
 
-                    // 동일한 값인 동안 left와 right을 이동하여 중복을 방지한다.
-                    while (left < right && nums[left] == nums[left + 1]) {
+                    // right 포인터 중복 처리
+                    while(left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+
+                    // left 포인터 중복 처리
+                    while(left < right && nums[left] == nums[left + 1]) {
                         left++;
                     }
-
-                    while (left < right && nums[right] == nums[right - 1]) {
-                        right--; 
-                    }
-
-                    // left와 right를 함께 이동하여 탐색 범위를 줄인다.
-                    left++;
+                    
                     right--;
+                    left++;
                 }
             }
         }
 
-        return new ArrayList<>(answer);
+        return result;
     }
 }
