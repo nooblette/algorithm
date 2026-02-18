c3dad5b10cfee77a9f1b22809234539bef1f8804
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2026-02-18 11:21:05 +0900

    Update 0073-set-matrix-zeroes.java

--- leetcode/0073-set-matrix-zeroes.java
+++ leetcode/0073-set-matrix-zeroes.java
@@ -3,27 +3,50 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
 
-        // 최초 값이 0인 위치 저장
-        // 2차원 배열이 아니라 1차원 배열 두 개를 선언하여 공간 복잡도를 O(m + n)으로 개선
-        boolean[] rows = new boolean[m];
-        boolean[] cols = new boolean[n];
-
+        // 1. 첫번째 행과 열에 0이 있었는지 확인
+        boolean isFirstRowZero = false;
+        for(int i = 0; i < n; i++) {
+            if(matrix[0][i] == 0) {
+                isFirstRowZero = true;
+                break;
+            }
+        }
+        
+        boolean isFirstColZero = false;
         for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
+            if(matrix[i][0] == 0) {
+                isFirstColZero = true;
+                break;
+            }
+        }
+
+        // 2. 두번째 행과 열부터 탐색 0인 부분이 있다면 첫번째 행과 열을 변경 (1번 케이스와 탐색 범위가 서로 다름)
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
                 if(matrix[i][j] == 0) {
-                    rows[i] = true;
-                    cols[j] = true;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
 
-        // 기존 배열을 0으로 변경
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                // 해당 행이나 열에 0인 값이 있다면 그 행, 열은 모두 0으로 만든다.
-                if(rows[i] || cols[j]) {
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(matrix[0][j] == 0 || matrix[i][0] == 0) {
                     matrix[i][j] = 0;
                 }
+            }
+        }
+
+        // 1번 케이스에서 기록했던 최초 0 처리
+        if(isFirstRowZero) {
+            for(int i = 0; i < n; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        if(isFirstColZero) {
+            for(int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
