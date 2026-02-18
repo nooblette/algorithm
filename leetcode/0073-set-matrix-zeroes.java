02f310d7c53650dc7b7fde9a06a157d5f534611c
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2026-02-18 10:45:14 +0900

    Update 0073-set-matrix-zeroes.java

--- leetcode/0073-set-matrix-zeroes.java
+++ leetcode/0073-set-matrix-zeroes.java
@@ -1,65 +1,45 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
+        // 최초 0인 인덱스 저장
+        List<List<Integer>> indexes = new ArrayList<>();
 
-        // 첫번째 행에 0이 있었는지 최초 정보 기록
-        boolean hasFirstRowZero = false;
-        for(int i = 0; i < n; i++) {
-            if(matrix[0][i] == 0) {
-                hasFirstRowZero = true;
-            }
-        }
-
-        // 첫번째 열에 0이 있었는지 최초 정보 기록
-        boolean hasFirstColZero = false;
-        for(int i = 0; i < m; i++) {
-            if(matrix[i][0] == 0) {
-                hasFirstColZero = true;
-            }
-        }
-
-        // matrix 배열을 탐색
-        for(int i = 1; i < m; i++) {
-            for(int j = 1; j < n; j++) {
-                // 0이라면
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
                 if(matrix[i][j] == 0) {
-                    // 첫번째 행, 열 값을 0으로 변경하여 대상 행, 열임을 마킹
-                    matrix[0][j] = 0;
-                    matrix[i][0] = 0;
+                    indexes.add(List.of(i, j));
                 }
             }
         }
 
-        // 첫번째 행에서 0인 열이 있다면, 동일한 열을 모두 0으로 변경
-        for(int i = 1; i < n; i++) {
-            if(matrix[0][i] == 0) {
-                for(int j = 1; j < m; j++) { 
-                    matrix[j][i] = 0;
-                }
-            }
-        }
+        for(List<Integer> index : indexes) {
+            int i = index.get(0);
+            int j = index.get(1);
 
-        // 첫번째 열에서 0인 행이 있다면, 동일한 행을 모두 0으로 변경
-        for(int i = 1; i < m; i++) {
-            if(matrix[i][0] == 0) {
-                for(int j = 1; j < n; j++) { 
-                    matrix[i][j] = 0;
-                }
+            // 상
+            while(i >= 0) {
+                matrix[i][j] = 0;
+                i--;
             }
-        }
+            i = index.get(0);
 
-        // 최초 정보를 기반으로 첫번째 행에 0이 있었다면 첫번째 행을 0으로 변경
-        if(hasFirstRowZero) {
-            for(int i = 0; i < n; i++) {
-                matrix[0][i] = 0;
+            // 하
+            while(i < matrix.length) {
+                matrix[i][j] = 0;
+                i++;
             }
-        }
+            i = index.get(0);
 
-        // 최초 정보를 기반으로 첫번째 열에 0이 있었다면 첫번째 열을 0으로 변경
-        if(hasFirstColZero) {
-            for(int i = 0; i < m; i++) {
-                matrix[i][0] = 0;
+            // 좌
+            while(j >= 0) {
+                matrix[i][j] = 0;
+                j--;
+            }
+            j = index.get(1);
+
+            // 우
+            while(j < matrix[0].length) {
+                matrix[i][j] = 0;
+                j++;
             }
         }
     }
