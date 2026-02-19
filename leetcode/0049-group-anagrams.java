98e1dbd912ea7797503eae02746b1b1c34653c17
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2026-02-19 15:45:10 +0900

    Update 0049-group-anagrams.java

--- leetcode/0049-group-anagrams.java
+++ leetcode/0049-group-anagrams.java
@@ -1,20 +1,17 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // Anagram 여부를 판단하기 위해 각 문자열을 정렬한 결과를 key, 각 문자열을 value로 갖는 Map 선언
+        // 정렬된 문자열을 key, 값 목록을 value로 갖는 Map
         Map<String, List<String>> map = new HashMap<>();
 
         for(String str : strs) {
-            // 정렬한 문자열 생성
-            char[] strArray = str.toCharArray();
-            Arrays.sort(strArray);
-            String ordered = new String(strArray);
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
 
-            // 문자열 목록 추가
-            List<String> soruces = map.getOrDefault(ordered, new ArrayList<>());
-            soruces.add(str);
+            String sorted = String.valueOf(chars);
 
-            // Map 갱신
-            map.put(ordered, soruces);
+            List<String> values = map.getOrDefault(sorted, new ArrayList<>());
+            values.add(str);
+            map.put(sorted, values);
         }
 
         return new ArrayList<>(map.values());
