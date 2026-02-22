4595384fac41a1172e1a302d9a18281b4c83083b
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2026-02-22 15:20:22 +0900

    Update 0334-increasing-triplet-subsequence.java

--- leetcode/0334-increasing-triplet-subsequence.java
+++ leetcode/0334-increasing-triplet-subsequence.java
@@ -1,27 +1,21 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        // 증가하는 Subsequence(nums[i] < nums[j] < nums[k])를 만족하는 두 개의 인덱스(i, j)를 기록
-        int i = 0;
-        int j = -1;
+        // 인덱스가 아닌 값 자체를 사용
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
 
-        for(int k = 1; k < nums.length; k++) {
-            // j가 유효(nums[i] < nums[j])한 경우 초기화
-            if(j == -1 && nums[i] < nums[k]) {
-                j = k;
-                continue;
-            }
-
-            // j가 유효하면서 nums[j] < nums[k]를 만족하는 k가 나온다면 Tripet을 완성하므로 탐색을 종료한다.
-            if(j != -1 && nums[j] < nums[k]) {
+        for(int num : nums) {
+            // first보다 작거나 값은 값이 나온다면 갱신한다.
+            // second는 갱신 이전 fisrt에 대해 fisrt < second를 만족하는 유효한 값일 수 있다.
+            // 따라서 다음 num에 대해 increasingTriplet을 만족할 수 있으므로 초기화하면 안된다. (이전 탐색 결과를 기록하고 있어야한다.)
+            if(num <= first) {
+                first = num;
+            } else if(num <= second) {
+                // first < num <= second를 만족하는 경우, fisrt < second를 유지하기 위해 second를 num으로 갱신한다.
+                second = num;
+            } else {
+                // first < second < num을 만족하는 경우, increasingTriplet를 찾았으므로 true 반환
                 return true;
-            } else if(j != -1 && nums[i] < nums[k] && nums[k] < nums[j]) {
-                // j가 유효하면서 nums[i]보다 크고 nums[j]보다 작은 nums[k]가 등장한다면 nums[i] < nums[j]를 유지하기 위해 j를 갱신한다.
-                j = k;
-            } else if(nums[i] > nums[k]) {
-                // nums[i]보다 작은 nums[k]가 등장한다면 i를 갱신한다.
-                // 하지만 j는 갱신 이전 i에 대해 nums[i] < nums[j]를 만족하는 유효한 포인터일 수 있다.
-                // 따라서 다음 nums[k]에 대해 increasingTriplet을 만족할 수 있으므로 j를 초기화하면 안된다. (이전 탐색 결과를 기록하고 있어야한다.)
-                i = k;
             }
         }
 
