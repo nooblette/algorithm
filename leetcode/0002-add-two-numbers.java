fe122c669e51b916efe6447b8960b499cfc1de8d
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2026-02-23 22:19:51 +0900

    Update 0002-add-two-numbers.java

--- leetcode/0002-add-two-numbers.java
+++ leetcode/0002-add-two-numbers.java
@@ -15,39 +15,22 @@ class Solution {
 
         // 올림 수 처리
         int carry = 0;
-        while(l1 != null || l2 != null) {
-            int val1 = 0;
-            if(l1 != null) {
-                val1 = l1.val;
-                l1 = l1.next;
-            }
-
-            int val2 = 0;
-            if(l2 != null) {
-                val2 = l2.val;
-                l2 = l2.next;
-            }
+        while(l1 != null || l2 != null || carry == 1) {
+            // null 처리를 간결하게 개선
+            int val1 = l1 != null ? l1.val : 0;
+            int val2 = l2 != null ? l2.val : 0;
             
             int sum = val1 + val2 + carry;
 
-            // 두 노드의 값이 두 자릿수라면 carry를 갱신하고 일의 자릿수만 사용한다.
-            if(sum >= 10) {
-                carry = 1;
-                sum -= 10;
-            } else {
-                // 두 노드의 값이 한 자릿수라면 carry를 초기화한다.
-                carry = 0;
-            }
-
-            cur.next = new ListNode(sum);
+            // 두 노드 합 계산시 carry 처리 개선
+            carry = sum / 10;
+            cur.next = new ListNode(sum % 10);
             cur = cur.next;
-        }
 
-        // 마지막 carry가 존재한다면 처리
-        if(carry == 1) {
-            cur.next = new ListNode(carry);
+            l1 = l1 != null ? l1.next : null;
+            l2 = l2 != null ? l2.next : null;
         }
-
+        
         return root.next;
     }
 }
